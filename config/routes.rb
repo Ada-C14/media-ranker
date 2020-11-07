17941f5d61a7cb04d9f9e7a9e1aa91ca5fe62d15
Alice D <alicedilanchian@gmail.com>
2020-11-07 14:27:23 -0500

    Work controller; methods index show update edit destroy new create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'work/index'
+  get 'work/show'
+  get 'work/update'
+  get 'work/edit'
+  get 'work/destroy'
+  get 'work/new'
+  get 'work/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

f87fdef81cd8deb2e55c8372391fb721ca910444
Anna Kim <anna.kim93@gmail.com>
2020-11-09 16:42:18 -0500

    add view for works index; add partial to view all the categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,6 @@ work_upload_failures = []
 CSV.foreach(WORK_FILE, :headers => true) do |row|
   work = Work.new
 
-  # work.id = row['id']
   work.category = row['category']
   work.title = row['title']
   work.creator = row['creator']
@@ -32,15 +31,5 @@ end
 
 puts "Added #{Work.count} work records"
 puts "#{work_upload_failures.size} works failed to save"
-
-
-# Since we set the primary key (the ID) manually on each of the
-# tables, we've got to tell postgres to reload the latest ID
-# values. Otherwise when we create a new record it will try
-# to start at ID 1, which will be a conflict.
-# puts "Manually resetting PK sequence on each table"
-# ActiveRecord::Base.connection.tables.each do |t|
-#   ActiveRecord::Base.connection.reset_pk_sequence!(t)
-# end
 
 puts "done"

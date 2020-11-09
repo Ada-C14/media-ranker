a7ed448ebd1aa2ed3eef78bc8f3e8c5201f89a09
Anna Kim <anna.kim93@gmail.com>
2020-11-09 14:39:34 -0500

    added show action to WorksController; added empty files for views

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,11 @@
 class WorksController < ApplicationController
+  # Helper Methods
+  def work_not_found_error_notice
+    flash[:notice] = "Uh oh! That work does not exist..."
+    redirect_to work_path
+  end
+
+  #########################################################
 
   def index
     @works = Work.all
@@ -8,4 +15,12 @@ class WorksController < ApplicationController
     @work = Work.new
   end
 
+  def show
+    work_id = params[:id].to_i
+    @work = Work.find_by(id: work_id)
+    if @work.nil?
+      work_not_found_error_notice
+      return
+    end
+  end
 end

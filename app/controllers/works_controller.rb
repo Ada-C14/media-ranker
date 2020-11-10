a79a691f91ea6ee5af1de485894d55ede329feff
Kareha Agesa <kmaagesa@gmail.com>
2020-11-09 22:09:07 -0800

    Works show page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,9 +2,17 @@ class WorksController < ApplicationController
 
   def index
     @albums = Work.all.where(category: "album")
-    @books = Work.all.where(category: "book") 
+    @books = Work.all.where(category: "book")
+    @movies = Work.all.where(category: "movie")
   end
 
   def show
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
+
+    # if @work.nil?
+    #   # here need to redirect to an error page
+    # end
+
   end
 end

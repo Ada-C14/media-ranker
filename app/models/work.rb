4feeb6b0f762918fa080d95cbcf62c5c1508b28c
Anna Kim <anna.kim93@gmail.com>
2020-11-10 15:33:35 -0500

    add tests for instantiation with correct fields; added validations to Work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,13 @@
 class Work < ApplicationRecord
 
+  validates :category,
+            presence: true,
+            inclusion: { in: %w(movie book album), message: "Category must be a movie, book, or album." }
+  validates :title, :creator, :description, presence: true
+  validates :publication_year,
+            presence: true,
+            :numericality => { :greater_than_or_equal_to => 0 }
+
   def self.media_spotlight
     Work.all.limit(1)[0]
   end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,51 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  let (:work) {
+    Work.create(
+        category: 'album',
+        title: 'ctrl',
+        creator: 'sza',
+        publication_year: 2017,
+        description: 'Top Dawg Entertainment and RCA Records'
+    )
+  }
+
+  it 'can be instantiated with the required fields' do
+    expect(work.valid?).must_equal true
+
+    work = Work.first
+    [:category, :title, :creator, :publication_year, :description].each do |field|
+      expect(work).must_respond_to field
+    end
+  end
+
+  describe 'relationships' do
+
+  end
+
+  describe 'validations' do
+
+  end
+
+  describe 'custom methods' do
+
+    it 'Work.media_spotlight: can get the top voted item' do
+
+    end
+
+    it 'Work.media_spotlight: responds with success hmmm idk' do
+
+    end
+
+    it 'Work.top_ten(category): can get top ten items for each category' do
+
+    end
+
+    it 'Work.top_ten(category): can get up to 10 items if there are less than 10' do
+
+    end
+
+  end
 end

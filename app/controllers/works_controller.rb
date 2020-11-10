c65cdc60aa193c6b5fac2b344fc3908596d2424d
Lina Do <l.do5147@gmail.com>
2020-11-10 12:57:07 -0800

    added index and show action methods for works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,40 @@
 class WorksController < ApplicationController
+
+  def index
+    @works = Work.all
+  end
+
+  def show
+    work_id = params[id:]
+    @work = Work.find_by(id: work_id)
+
+    if @work.nil?
+      render file: "#{Rails.root}/public/404.html", status: :not_found
+      return
+    end
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+
 end

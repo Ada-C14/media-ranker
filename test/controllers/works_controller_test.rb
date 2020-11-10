2f95d5dca446875533b6d0664b6b59f34f0fccee
Anna Kim <anna.kim93@gmail.com>
2020-11-10 17:50:13 -0500

    added WorksController #index tests

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -2,8 +2,27 @@ require "test_helper"
 
 describe WorksController do
 
-  describe 'index' do
+  let (:work) {
+    Work.create(
+        category: 'album',
+        title: 'ctrl',
+        creator: 'sza',
+        publication_year: 2017,
+        description: 'Top Dawg Entertainment and RCA Records'
+    )
+  }
 
+  describe 'index' do
+    it 'responds with success when there are works saved' do
+      work
+      get works_path
+      must_respond_with :success
+    end
+
+    it 'responds with success when there are no works saved' do
+      get works_path
+      must_respond_with :success
+    end
   end
 
   describe 'show' do

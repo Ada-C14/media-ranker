0c6f389e3ff709cf9c959305a18cad8d351ea092
roshni-patel <rpatel01@wesleyan.edu>
2020-11-10 15:48:26 -0600

    Working on works controller actions, added strong params, and set up table for index view

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,42 @@
 class WorksController < ApplicationController
   def index
-    @works = Work.all
+    # @works = Work.all.order(:category)
+    @albums = Work.all.where(category: "album")
+    @books = Work.all.where(category: "book")
+    @movies = Work.all.where(category: "movie")
   end
 
+  def new
+    @work = Work.new
+  end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+    end
+  end
+
+
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      flash[:success] = "#{@work.name} was successfully added!"
+      redirect_to work_path
+      return
+    else
+      flash.now[:error] = "Something happened. #{@work.name} was not added."
+      render :new, status: :bad_request
+      return
+    end
+  end
+
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end

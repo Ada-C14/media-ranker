701d26ca0bc9fc43ff6ae2284499304d5a3e8d60
Kareha Agesa <kmaagesa@gmail.com>
2020-11-09 20:41:26 -0800

    seed Work data, add Work routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'works/index'
-  get 'works/show'
+  resources :works
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

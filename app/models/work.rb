5f836c8f8b5a2c380cae546cd0cc9271caad236d
Sophie Messing <sophie.e.messing@gmail.com>
2020-11-09 19:07:03 -0800

    wrote some business logic methods, not sure if they work

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,7 +1,19 @@
 class Work < ApplicationRecord
 
   def category
-    @books
+    @books = @works.where(category: "book")
+    @albums = @works.where(category: "album")
+    @movies = @works.where(category: "movie")
+  end
+
+  def top_10
+    @top_books = @books.sample(10)
+    @top_movies = @movies.sample(10)
+    @top_albums = @books.sample(10)
+  end
+
+  def spotlight
+    @spotlight = @works.sample(1)
   end
 
 end

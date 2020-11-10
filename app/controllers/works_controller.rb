4eabb33e55849c91de05d7c5a71dcd7675a9e843
Anna Kim <anna.kim93@gmail.com>
2020-11-09 21:54:38 -0500

    add media controller create action

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,8 +1,12 @@
 class WorksController < ApplicationController
   # Helper Methods
-  def work_not_found_error_notice
+  def not_found_error_notice
     flash[:notice] = "Uh oh! That work does not exist..."
     redirect_to work_path
+  end
+
+  def not_saved_error_notice
+    flash[:notice] = "Uh oh! That did not save correctly."
   end
 
   #########################################################
@@ -15,7 +19,7 @@ class WorksController < ApplicationController
     work_id = params[:id].to_i
     @work = Work.find_by(id: work_id)
     if @work.nil?
-      work_not_found_error_notice
+      not_found_error_notice
       return
     end
   end
@@ -26,6 +30,15 @@ class WorksController < ApplicationController
 
   def create
     @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to work_path(@work.id)
+      return
+    else
+      not_saved_error_notice
+      render :new
+      return
+    end
   end
 
   private

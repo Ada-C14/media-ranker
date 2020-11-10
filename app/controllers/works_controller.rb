d1ab4e7ac3710105343cd981293bae4a4b0d897f
Anna Kim <anna.kim93@gmail.com>
2020-11-09 21:52:38 -0500

    add strong params

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -11,10 +11,6 @@ class WorksController < ApplicationController
     @works = Work.all
   end
 
-  def new
-    @work = Work.new
-  end
-
   def show
     work_id = params[:id].to_i
     @work = Work.find_by(id: work_id)
@@ -23,4 +19,19 @@ class WorksController < ApplicationController
       return
     end
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+
 end

dde51e6d915fff1ec3decbe674c5164f2a6c7ad2
Kareha Agesa <kmaagesa@gmail.com>
2020-11-09 22:18:06 -0800

    a passing works show test and a test i dont know how to implement yet

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,14 +1,50 @@
 require "test_helper"
 
 describe WorksController do
-  it "must get index" do
-    get works_index_url
-    must_respond_with :success
+
+  describe "root" do
+    it "can get the root path" do
+      get root_path
+      must_respond_with :success
+    end
   end
 
-  it "must get show" do
-    get works_show_url
-    must_respond_with :success
+  describe "index" do
+    it "must get index" do
+      get works_url
+      must_respond_with :success
+    end
+
+    it "doesnt break when movies, albums, or books are nil" do
+      skip
+      # idk how to test this
+    end
+  end
+
+  describe "show" do
+    before do
+      @work = Work.create!(
+        category: "album",
+        title: "test",
+        creator: "The Testor",
+        publication_year: 2020,
+        description: "We love a good test")
+    end
+
+    let (:bad_url){
+      -99999
+    }
+
+    it "must get show" do
+      get works_url(@work.id)
+      must_respond_with :success
+    end
+
+    it "redirects to error page for invalid work" do
+      skip
+      get works_url(bad_url)
+      # must redirect somewhereeeeee
+    end
   end
 
 end

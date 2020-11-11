8b721f821a6f5743a55d619a3031e9d36d95bda5
marks214 <aimee.oz.x@gmail.com>
2020-11-10 22:05:17 -0800

    seeded data - used ride-share-rails seeds.rb file as template

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,31 +1,33 @@
 class WorksController < ApplicationController
 
   def index
-
+    @works = Work.all
   end
 
   def show
+    @work = Work.find_by(id: params[:id])
 
+    if @work.nil?
+      redirect_to works_path
+    end
   end
 
   def new
-
+    @work = Work.new
   end
 
-  def create
+  def create; end
 
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
-
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  def destroy
-
-  end
-
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,7 +1,19 @@
-require "test_helper"
+require 'test_helper'
 
 describe WorksController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it 'index' do
+    get works_path
+    must_respond_with :success
+  end
+
+  it 'show' do
+    get work_path
+    must_respond_with :success
+  end
+
+  it 'new' do
+    get new_work_path
+    must_respond_with :success
+  end
+
 end

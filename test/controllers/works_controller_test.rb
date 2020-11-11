363eb635b6e9b988d6618ab97f2bf3e39329ba34
Anna Kim <anna.kim93@gmail.com>
2020-11-10 19:15:43 -0500

    added WorksController #create tests

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -12,6 +12,30 @@ describe WorksController do
     )
   }
 
+  let (:work_hash) {
+    {
+        work: {
+          category: 'album',
+          title: 'ctrl',
+          creator: 'sza',
+          publication_year: 2017,
+          description: 'Top Dawg Entertainment and RCA Records'
+        }
+    }
+  }
+
+  let (:invalid_work_hash) {
+    {
+        work: {
+            category: 'album',
+            title: 'ctrl',
+            creator: 'sza',
+            publication_year: -1600,
+            description: 'Top Dawg Entertainment and RCA Records'
+        }
+    }
+  }
+
   describe 'index' do
     it 'responds with success when there are works saved' do
       work
@@ -42,11 +66,33 @@ describe WorksController do
   end
 
   describe 'new' do
-
+    it "responds with success" do
+      get new_work_path
+      must_respond_with :success
+    end
   end
 
   describe 'create' do
+    it "can create a new work with valid information and redirect" do
+      expect {
+        post works_path, params: work_hash
+      }.must_change "Work.count", 1
 
+      new_work = Work.find_by(title: work_hash[:work][:title])
+      expect(new_work.category).must_equal work_hash[:work][:category]
+      expect(new_work.creator).must_equal work_hash[:work][:creator]
+      expect(new_work.publication_year).must_equal work_hash[:work][:publication_year]
+      expect(new_work.description).must_equal work_hash[:work][:description]
+
+      must_respond_with :redirect
+      must_redirect_to work_path(new_work.id)
+    end
+
+    it "does not create a work if the form data violates Work validations" do
+      expect {
+        post works_path, params: invalid_work_hash
+      }.wont_change "Work.count"
+    end
   end
 
   describe 'edit' do

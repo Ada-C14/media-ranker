3a039d47b77d6fe87a29014674b600a4251e0c3f
Li Dai <li.lea.dai@gmail.com>
2020-11-11 13:29:31 -0800

    updated validations allow only unique title

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -9,6 +9,19 @@ describe Work do
         publication_year: 2020,
         description: "testing")
   }
+  describe "validations" do
+    it "is invalid without a title" do
+      @work.title = nil
+      result = @work.valid?
+      expect(result).must_equal false
+      expect(@work.errors.messages).must_include :title
+    end
+
+    it "is invalid with a non-unique title" do
+      second_work.title = "test"
+      result = second_work.valid?
+      expect(result).must_equal false
+    end
 
 
-end
+  end

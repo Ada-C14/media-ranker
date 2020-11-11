6941c4a60db9bf395e3656d8eb1e16dcc8d27521
Hanh Solo <minhhanhnyc@gmail.com>
2020-11-10 20:42:31 -0800

    Updated index method in pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def index
-    
+    @works = Work.all
+    @albums = Work.select{ |work| work.category == 'album'}
+    @books = Work.select{ |work| work.category == 'book'}
+    @movies = Work.select{ |work| work.category == 'movie'}
   end
 end

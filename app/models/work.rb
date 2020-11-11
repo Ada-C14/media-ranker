14e4a8424d38958b02bd2d0c8f45f7d935f6d57d
Jasmine <jasylop@gmail.com>
2020-11-11 15:05:30 -0800

    added custom methods for top_ten and spotlight

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,12 @@
 class Work < ApplicationRecord
   validates :category, presence: true
   validates :title, presence: true, uniqueness: true
+
+  def self.spotlight
+    return Work.all.sample
+  end
+
+  def self.top_ten(category)
+    return Work.where(category: category).sample(10)
+  end
 end

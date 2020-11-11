72d7dfc586aeaa187b26a3939d241f961c59b513
Jasmine <jasylop@gmail.com>
2020-11-10 17:20:36 -0800

    added index, show, new, and create actions

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,44 @@
 class WorksController < ApplicationController
+
+  def index
+    @works = Work.all
+  end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      redirect_to works_path, error: "Work not found"
+      return
+    end
+
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+
+      redirect_to work_path(@work), success: "Successfully updated #{@work.category} #{@work.id}"
+      return
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end

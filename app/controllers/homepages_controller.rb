ff8c79e399e663abf0bd667802ebe79f3a3d7a03
Lina Do <l.do5147@gmail.com>
2020-11-10 19:09:02 -0800

    home page view, sample of 10 for each category

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,5 +1,7 @@
 class HomepagesController < ApplicationController
   def index
-
+    @works_book = Work.where(category: 'book').sample(10)
+    @works_album = Work.where(category: 'album').sample(10)
+    @works_movie = Work.where(category: 'movie').sample(10)
   end
 end

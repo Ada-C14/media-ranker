3b51eed09136dd1007dffd7810046e6455382dc7
Beatrice T <be@trice.codes>
2020-11-11 15:19:05 -0800

    Created db seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+WORKS_FILE = Rails.root.join('db', 'works-seeds.csv')
+puts "Loading raw works data from #{WORKS_FILE}"
+
+works_failures = []
+CSV.foreach(WORKS_FILE, :headers => true) do |row|
+  work = Work.new
+  work.category = row['category']
+  work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year'].to_i
+  work.description = row['description']
+  if work.save
+    puts "Created work: #{work.inspect}"
+  else
+    works_failures << work
+    puts "Failed to save work: #{work.inspect}"
+  end
+end
+
+puts "Added #{Work.count} driver records"
+puts "#{works_failures.length} drivers failed to save"
+
+puts "That's all, folks"

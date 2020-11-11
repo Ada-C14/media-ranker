00624930a5c5fa7e44a2ecb3ad8c8ce430b91bd6
Kareha Agesa <kmaagesa@gmail.com>
2020-11-10 17:20:32 -0800

    a home page

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,18 @@
 class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: true
+
+  def self.select_spotlight
+    # will this be in votes model?
+    # this will change to pick the work with the highest votes
+    spotlight = self.all.sample
+    return spotlight
+  end
+
+  def self.select_top_ten
+    movies = Work.where(category: "movie")
+    albums = Work.where(category: "album")
+    books = Work.where(category: "book")
+    return movies, albums, books
+  end
+
 end

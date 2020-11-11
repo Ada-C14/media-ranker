5b96d2ec977f7a311ccedca27e1d44f649bb44ef
India Kato <india.kato@gmail.com>
2020-11-11 10:50:37 -0800

    added index and show action to works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,21 @@
 class WorksController < ApplicationController
+    def index
+        @works = Works.all
+    end
+
+    def show
+        @work = Work.find_by(id: params[:id])
+
+        if @work.nil?
+            head :not_found
+            return
+        end 
+    end
+
+
+    private
+    
+    def work_params
+        return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+    end
 end

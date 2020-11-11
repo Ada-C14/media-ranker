2635668757c0cef456df977a32142b8b9f37f29c
Jasmine <jasylop@gmail.com>
2020-11-10 17:19:04 -0800

    added tests for index & show for works controller

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 describe WorksController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do
+    it "can get index" do
+      get works_path
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "can get show page" do
+      # use book from fixtures
+      get work_path(works(:hp1))
+      must_respond_with :success
+    end
+
+    it "redirects to index with message if work doesn't exist" do
+      get work_path(-1)
+      must_respond_with :redirect
+      expect(flash[:error]).wont_be_nil
+    end
+  end
+
+  describe "new" do
+
+  end
+
+  describe "create" do
+
+  end
+
+  describe "edit" do
+
+  end
+
+  describe "update" do
+
+  end
+
+  describe "destroy" do
+
+  end
 end

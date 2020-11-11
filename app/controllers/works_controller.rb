0caa0b087891da60d8069006a49558f6488137ae
Kareha Agesa <kmaagesa@gmail.com>
2020-11-11 13:00:23 -0800

    adding some routes for users

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,5 @@
 class WorksController < ApplicationController
 
-  # session
   def index
     @albums = Work.all.where(category: "album")
     @books = Work.all.where(category: "book")
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   root to: 'homepages#index'
+
+  resources :users, only: [:show, :index]
+  get '/login', to: 'users#login_form', as: 'login'
+
   resources :works
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

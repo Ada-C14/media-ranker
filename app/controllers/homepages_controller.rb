7805ad417402b12c93e8ce4d45ab68b5cbad9fa9
roshni-patel <rpatel01@wesleyan.edu>
2020-11-10 19:56:38 -0600

    Added methods for media spotlight and top ten, updated views for homepage

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,4 +1,3 @@
 class HomepagesController < ApplicationController
   def index; end
-
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,9 @@
 class Work < ApplicationRecord
+  def self.spotlight
+    return Work.all.sample
+  end
+
+  def self.top_ten(category)
+    return Work.where(category: category).sample(10)
+  end
 end

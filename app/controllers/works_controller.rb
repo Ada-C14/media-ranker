c70bc7f65b9cf06f735f46e280213f92885d36e5
India Kato <india.kato@gmail.com>
2020-11-11 12:17:49 -0800

    added @albums, @books, @movies to works controller index

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,10 @@
 class WorksController < ApplicationController
     def index
         @works = Work.all
+
+        @albums = @works.where(category: 'album')
+        @books = @works.where(category: 'book')
+        @movies = @works.where(category: 'movie')
     end
 
     def show

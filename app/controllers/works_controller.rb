a13cec73fcffb836846041e94c406bfd0d4e76c9
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-10 18:10:50 -0800

    add index and show methods for trips controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,8 +1,16 @@
 class WorksController < ApplicationController
   def index
+    @works = Work.all
   end
 
   def show
+    work_id = params[:id].to_i
+    @work = Work.find_by(id: work_id)
+
+    if @work.nil?
+      head :not_found
+      return
+    end
   end
 
   def new

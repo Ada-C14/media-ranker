ec70f09de13c494ebe3e240dd4e64ccf67f572ec
Christina Minh <christina.minh@gmail.com>
2020-11-11 15:08:07 -0800

    Added UsersController#index, #login, #create, and #show. Deleted unnecessary views from User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,38 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
-  def new
+  def login
+    @user = User.new
   end
 
   def create
+    @user = User.new(users_params)
+
+    if @user.save
+      flash[:success] = "Successfully created new user #{@user.username} with ID #{@user.id}"
+      redirect_to root_path
+      return
+    else
+      flash.now[:error] = "A problem occurred: Could not log in"
+      render :login
+      return
+    end
   end
 
   def show
+    @user = User.find_by(id: params[:id].to_i)
+
+    if @user.nil?
+      head :not_found
+      return
+    end
+  end
+
+  private
+
+  def users_params
+    return params.require(:user).permit(:username)
   end
 end

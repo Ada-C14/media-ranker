7f9c83ed53b3ba652872eed1a756088d57cba7b9
Mackenzie <mvlofthus@gmail.com>
2020-11-11 14:31:35 -0800

    made homepage controller, started other roots

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,19 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      redirect_to works_path, status: :temporary_redirect
+      return
+    end
+  end
+
+  def new
+    @work = Work.new
+  end
+
+
 end

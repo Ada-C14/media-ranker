948a7cfadec53c903541016a3aae33baf1a815a1
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-10 21:37:26 -0800

    added homepage controller and works routes

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,34 @@
 class WorksController < ApplicationController
+
+  def index
+    @works = Work.all
+  end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to work_path(@work.id)
+    end
+  end
+
+
+
+
+
+  private
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end

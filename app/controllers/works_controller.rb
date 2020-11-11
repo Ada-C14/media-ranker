ecfbec8b9a0c0cd48a977557616eb2cc0faf69f5
India Kato <india.kato@gmail.com>
2020-11-11 13:29:41 -0800

    added edit and update actions to works controller, made edit.html.erb for works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -20,6 +20,29 @@ class WorksController < ApplicationController
         @work = Work.new
     end
 
+    def edit
+        @work = Work.find_by(id: params[:id])
+
+        if @work.nil?
+            redirect_to works_path
+            return
+        end
+    end
+
+    def update
+        @work = Work.find_by(id: params[:id])
+
+        if @work.nil?
+            redirect_to works_path
+            return
+        elsif @work.update(work_params)
+            redirect_to work_path(@work)
+            return
+        else
+            render :edit
+            return
+        end
+    end
     
     private
     

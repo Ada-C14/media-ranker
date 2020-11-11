035544870e74e2162aa29b005adf44f073354d6b
Alice D <alicedilanchian@gmail.com>
2020-11-11 18:34:11 -0500

    sample 10 methods for albums, books, and spotlights in homepages_controller, matching changes in homepages index erb

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,5 +1,22 @@
 class HomepagesController < ApplicationController
   def index
+    #@works is an array
     @works = Work.all
+
+    @albums = @works.each do |work|
+      if work[0] == "album"
+        @albums += work
+      end
+    end
+    @albums = @albums.sample(10)
+
+    @books = @works.each do |work|
+      if work[0] == "book"
+        @books += work
+      end
+    end
+    @books = @books.sample(10)
+
+    @spotlights = @works.sample(10)
   end
 end

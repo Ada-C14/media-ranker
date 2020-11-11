ea346a284c53d16ada4839d9691878cbaf7cd049
Hanh Solo <minhhanhnyc@gmail.com>
2020-11-10 20:47:08 -0800

    Updated routes for root and works

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'votes/index'
-  get 'users/index'
-  get 'works/index'
-  get 'pages/index'
+  resources :works
+  root 'pages#index'
+  # get 'votes/index'
+  # get 'users/index'
+  # get 'works/index'
+  # get 'pages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

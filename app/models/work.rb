611bcbc5f25c885287df9ba468e199bbb7782b09
Iris Lux <iris.lux0@gmail.com>
2020-11-10 22:03:45 -0800

    seed script - fixed typo in work and user models

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,4 @@
 class Work < ApplicationRecord
   has_many :votes
-  has_many :users, through: votes
+  has_many :users, through: :votes
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+WORK_FILE = Rails.root.join('db', 'seed_data', 'works.csv')
+puts "Loading raw work data from #{WORK_FILE}"
+
+work_failures = []
+CSV.foreach(WORK_FILE, :headers => true) do |row|
+  work = Work.new
+  work.id = row['id']
+  work.title = row['title']
+  work.description = row['description']
+  work.creator = row['creator']
+  work.publication_date = row['publication_date']
+  work.category = row['category']
+  successful = work.save
+  if !successful
+    work_failures << work
+    puts "Failed to save work: #{work.inspect}"
+  else
+    puts "Created work: #{work.inspect}"
+  end
+end
+
+

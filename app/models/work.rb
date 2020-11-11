e300984bccecf9d7e880f2d81dced92801cecd62
Li Dai <li.lea.dai@gmail.com>
2020-11-11 13:37:39 -0800

    added validations tests for work

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,7 +2,7 @@ class Work < ApplicationRecord
   has_many :users, through: :votes
 
   validates :category, presence: true
-  validates :title, uniqueness: { case_sensitive: false }
+  validates :title, presence: true, uniqueness: { case_sensitive: false }
   # validates :creator, presence: true
   # validates :publication_year, numericality: { only_integer: true }
   # validates :description, presence: true
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -9,19 +9,23 @@ describe Work do
         publication_year: 2020,
         description: "testing")
   }
+
   describe "validations" do
     it "is invalid without a title" do
-      @work.title = nil
-      result = @work.valid?
+      work.title = nil
+      result = work.valid?
       expect(result).must_equal false
-      expect(@work.errors.messages).must_include :title
+      expect(work.errors.messages).must_include :title
     end
 
-    it "is invalid with a non-unique title" do
-      second_work.title = "test"
-      result = second_work.valid?
+    it "is invalid with a repeating title and validation is not case sensitive" do
+      Work.create!(
+          category: "album",
+          title: "Testing")
+      work.title = "testing"
+      result = work.valid?
       expect(result).must_equal false
     end
-
 
   end
+end

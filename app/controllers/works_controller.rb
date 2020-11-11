7df1a18d0ac8fcf13e267db8eac144077dcbb77d
Ana Parada <anakp07@gmail.com>
2020-11-10 16:17:40 -0800

    create the database and migrated, I also added RESTful routes for Works and began to work on Works Controller methods

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,19 @@
 class WorksController < ApplicationController
+
+  def index
+    @works = Work.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+
+  end
+
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,6 @@
 class Work < ApplicationRecord
+  validates :title, presence: true
+
+  has_many :votes
+  has_many :users, through: :votes
 end

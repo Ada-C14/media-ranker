d0812a3369ab94e6f6019b5a2c2271d5febc581a
Christina Minh <christina.minh@gmail.com>
2020-11-11 14:43:22 -0800

    Added UsersController index test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,13 +2,23 @@ require "test_helper"
 
 describe UsersController do
   describe "index" do
-    it "responds with success if users are saved" do
+    it "responds with success when users are saved" do
+      user = users()
 
+      get users_path
+
+      must_respond_with :success
     end
 
-    it "responds with success if no users are saved" do
+    it "responds with success when no users are saved" do
+      User.destroy_all
 
+      get users_path
+
+      must_respond_with :success
     end
   end
+
+
 
 end

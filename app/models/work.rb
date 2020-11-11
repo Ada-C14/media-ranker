7ae4e11da88804e569605c9496747ed1b83f98e3
Alice Boone <alicegbonel@gmail.com>
2020-11-11 15:30:04 -0800

    Create helper methods to load albums, books and movies

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,13 @@
 class Work < ApplicationRecord
+  def self.albums
+    where(category: "album")
+  end
 
+  def self.books
+    where(category: "book")
+  end
+
+  def self.movies
+    where(category: "movie")
+  end
 end

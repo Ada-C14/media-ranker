bfc03377ddc56e83aa9c8165852606774e1f6e82
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-10 20:18:30 -0800

    add all actions for works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -8,24 +8,59 @@ class WorksController < ApplicationController
     @work = Work.find_by(id: work_id)
 
     if @work.nil?
-      head :not_found
+      redirect_to works_path
       return
     end
   end
 
   def new
+    @work = Work.new
   end
 
   def create
+    work = Work.new(work_params)
+
+    if work.save
+      redirect_to work_path(work)
+    else
+      render :new, status: :bad_request
+    end
   end
 
   def edit
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      redirect_to root_path
+      return
+    end
   end
 
   def update
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    elsif @work.update(work_params)
+      redirect_to works_path
+      return
+    else
+      render :edit
+      return
+    end
   end
 
   def destroy
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    else
+      @work.destroy
+      redirect_to root_path
+    end
   end
 
   private

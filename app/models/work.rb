aba0f58d7da56a3dd44e53be205c3f80a8b1119b
MonaRahmani <monarahmanimehr@gmail.com>
2020-11-11 10:44:24 -0800

    new work validation & relationships added

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,14 @@
 class Work < ApplicationRecord
+
+  validates :title, presence: true
+  validates :category, presence: true
+  validates :creator, presence: true
+  validates :publication_year, presence: true
+
+  # relationship
+  has_many :votes
+  has_many :users, through: :votes
+
   def top_ten(category)
     top_ten_list = Work.where(category: category)
     return top_ten_list

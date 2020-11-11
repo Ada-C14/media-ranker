318087c241d8877736da8f8972928e0064bc2e65
MonaRahmani <monarahmanimehr@gmail.com>
2020-11-10 23:05:17 -0800

    some action method created for works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -7,12 +7,27 @@ class WorksController < ApplicationController
   end
 
   def show
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    end
   end
 
   def new
+    @work = Work.new
   end
 
   def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to root_path
+      return
+    else
+      flash.now[:error] = "something happend. not added"
+      render :new, status: :bad_request
+      return
+    end
   end
 
   def update
@@ -25,6 +40,7 @@ class WorksController < ApplicationController
   end
 
   private
+
   def work_params
     return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end

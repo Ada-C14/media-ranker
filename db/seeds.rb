924ba96e9a696190ba85deedd3dfb88e1b3f7b5f
Christina Minh <christina.minh@gmail.com>
2020-11-10 17:31:47 -0800

    Changed datatype of publication_year to integer to be able to seed work data from CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 require 'csv'
+require 'time'
 
 WORK_FILE = Rails.root.join('db', 'works-seeds.csv')
 puts "Loading generated work data from #{WORK_FILE}"

f1f0df88fd298eea65d764d4adba92b950f26be8
Christina Minh <christina.minh@gmail.com>
2020-11-11 14:16:52 -0800

    Created users.yml

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,39 +1,14 @@
 require "test_helper"
 
 describe UsersController do
-  it "must get index" do
-    get users_index_url
-    must_respond_with :success
-  end
+  describe "index" do
+    it "responds with success if users are saved" do
 
-  it "must get new" do
-    get users_new_url
-    must_respond_with :success
-  end
+    end
 
-  it "must get create" do
-    get users_create_url
-    must_respond_with :success
-  end
+    it "responds with success if no users are saved" do
 
-  it "must get show" do
-    get users_show_url
-    must_respond_with :success
-  end
-
-  it "must get edit" do
-    get users_edit_url
-    must_respond_with :success
-  end
-
-  it "must get update" do
-    get users_update_url
-    must_respond_with :success
-  end
-
-  it "must get destroy" do
-    get users_destroy_url
-    must_respond_with :success
+    end
   end
 
 end

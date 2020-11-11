b705f489f8ef3be2a9e34462b5c06856b71e3092
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-10 21:43:09 -0800

    Fully implemented CRUD for work controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -20,12 +20,46 @@ class WorksController < ApplicationController
     @work = Work.new(work_params)
     if @work.save
       redirect_to work_path(@work.id)
+      return
+    else
+      render :new
+      return
     end
   end
 
+  def edit
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    end
+  end
 
+  def update
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    elsif @work.update(work_params)
+      redirect_to work_path
+      return
+    else
+      render :edit, status: :bad_request
+      return
+    end
+  end
 
-
+  def destroy
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    else
+      @work.destroy
+      redirect_to work_path
+      return
+    end
+  end
 
   private
   def work_params

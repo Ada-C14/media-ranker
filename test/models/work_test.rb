aac0b5664e7b627707421521e94ca42bfb53cbef
Jasmine <jasylop@gmail.com>
2020-11-11 10:43:48 -0800

    added tests for all current validations

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  describe "validations" do
+    it "is valid when all fields are present" do
+      result = works(:hp1).valid?
+      expect(result).must_equal true
+    end
+
+    it "must have a category" do
+      result = works(:hp1)
+      result[:category] = nil
+      expect(result.valid?).must_equal false
+    end
+
+    it "must have a title" do
+      result = works(:hp1)
+      result[:title] = nil
+      expect(result.valid?).must_equal false
+    end
+
+    it "must have a unique title" do
+      book = works(:hp1)
+      book_copy = Work.new(category: "book", title: book.title)
+      expect(book_copy.valid?).must_equal false
+    end
+  end
+
+  describe "custom methods" do
+
+  end
+
+  describe "relationships" do
+    it "can have many users through votes" do
+
+    end
+
+    it "can have many votes" do
+
+    end
+  end
 end

6b163ea3fbef294008be0fb3b5b6c3b9cf483efa
Ren Carothers <rencarothers@gmail.com>
2020-11-11 19:50:47 -0800

    adds basic validations for work

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,12 @@
 class Work < ApplicationRecord
 
-  CATEGORIES = %w(album book movie) # Use me for checkboxes on form partials :)
+  CATEGORIES = %w(album book movie) # For checkboxes on form partials // validations
+
+  validates :category,  presence: true,
+            inclusion: { in: CATEGORIES }
+
+  validates :title, presence: true,
+            uniqueness: { scope: :category }
 
   def self.by_category(category)
     self.where(category: category).order(category: :desc) #TODO: change order to vote count when initialized

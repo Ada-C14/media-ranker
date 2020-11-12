2714aabbba5e7f3c0acd0918dbe05246b671e6dd
Pauline Chane <pauline.chane@gmail.com>
2020-11-11 17:08:47 -0800

    found a place to DRY the work model, tests don't break

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,14 +4,6 @@ class Work < ApplicationRecord
   # before validating
   before_validation {self.category = category.downcase if !self.category.nil?} # presence catches a nil entry
   # validations
-  validate :valid_title
   validates :category, inclusion: { in: %w[book movie album], message: 'must be book, movie, or album'}
-  validates :title, presence: true
-
-  def valid_title
-    same_category = Work.where(category: self.category).pluck(:title)
-    if same_category.include?(self.title)
-      errors[:title] << 'has already been taken'
-    end
-  end
+  validates :title, presence: true, uniqueness: {scope: :category, message: 'has already been taken'}
 end

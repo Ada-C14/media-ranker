4d8d5f81f09ffe6dced5588e05d47113790cd2d7
MonaRahmani <monarahmanimehr@gmail.com>
2020-11-11 23:30:22 -0800

    user controller actions show, index, loginform created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,33 @@
 class UsersController < ApplicationController
   def index
-
+    @users = User.all
   end
 
   def show
-
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      head :not_found
+      return
+    end
   end
 
+  def login_form
+    @user = User.new
+  end
+
+  def login
+  end
+
+  def logout
+  end
+
+
+
+
+
+  private
+
+  def user_params
+    return params.require(:user).permit(:username)
+  end
 end

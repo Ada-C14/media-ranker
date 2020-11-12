146a2e75af9032f0ad2b38d3bc495c251c36dd8c
MonaRahmani <monarahmanimehr@gmail.com>
2020-11-11 23:53:22 -0800

    user custom routes added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,9 @@ class UsersController < ApplicationController
   def logout
   end
 
+  def current
+  end
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # user
   resources :users, only: [:index, :show]
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
+
+  # work
   resources :works
+
+  # root
   root to: 'pages#index'
 end

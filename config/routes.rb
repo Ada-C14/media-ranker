530ccc96ca52785dde0d1d95e8c3e7484d23f08b
Emily Nagai <emnagai89@gmail.com>
2020-11-12 12:44:44 -0800

    refactored routes to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'works#index'
-  get '/works', to: 'works#index', as: 'works'
-  get '/works/:id', to: 'works#show', as: 'work'
+  resources :works
+  # get '/works', to: 'works#index', as: 'works'
+  # get '/works/new', to: 'works#new', as: 'new_work'
+  # post '/works', to: 'works#create'
+  #
+  # get '/works/:id', to: 'works#show', as: 'work'
+  # patch '/works/:id/edit', to: 'works#edit', as: 'edit_work'
+  # get '/works/:id', to: 'works#update'
+  # delete '/works/:id', to: 'works#destroy'
 
 end

f9d57057af813cc341f85a3de9fade0a52fdce44
India Kato <india.kato@gmail.com>
2020-11-12 15:42:55 -0800

    Added user logout action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,38 @@ class UsersController < ApplicationController
   end
 
   def login
+    user = User.find_by(username: params[:user][:username])
+
+    if user.nil?
+      #New User
+      user = User.new(username: params[:user][:username])
+      if ! user.save
+        flash[:error] = "Unable to login"
+        redirect_to root_path
+        return
+      end
+      flash[:welcome] = "Welcome #{user.username}"
+    else
+      #Existing User
+      flash[:welcome] = "Welcome back #{user.username}"
+    end
+
+    session[:user_id] = user.id
+    redirect_to root_path
   end
+
+  def logout
+    if session[:user_id]
+      user = User.find_by(id: session[:user_id])
+      unless user.nil?
+        session[:user_id] = nil
+        flash[:notice] = "Goodbye #{user.username}"
+      else
+        session[:user_id] = nil
+        flash[:notice] = "Error: User unknown"
+      end
+    else
+      flash[:error] = "You must be logged in to logout"
+    end
+    redirect_to root_path
 end

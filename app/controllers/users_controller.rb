01b4477fbf3f8d81bfdac2e8818e258139e2ec14
Anna Kim <anna.kim93@gmail.com>
2020-11-12 17:52:21 -0500

    add user_params private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,32 @@ class UsersController < ApplicationController
       end
   end
 
+  def not_saved_error_notice
+    flash.now[:notice] = "Uh oh! That did not save correctly. Please try again."
+  end
+
   #########################################################
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      # redirect_to user_path(@user.id)
+      return
+    else
+      not_saved_error_notice
+      render :new
+      return
+    end
+  end
+
+  # custom actions
   def login_form
     @user = User.new
   end
 
   def login
-    username = params[:user][:username]
+    username = user_params[:username]
     user = User.find_by(username: username)
 
     if user
@@ -31,4 +49,11 @@ class UsersController < ApplicationController
     redirect_to root_path
     return
   end
+
+  private
+
+  def user_params
+    return params.require(:user).permit(:username)
+  end
+
 end

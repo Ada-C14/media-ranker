b345eeacf0497a09447a29721d71354848495d3c
Beatrice T <be@trice.codes>
2020-11-11 21:22:57 -0800

    Added model logic for top ten and media spotlight functions

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -8,12 +8,5 @@ class WorksController < ApplicationController
       @works_hash[category] = Work.where(category: category)
     end
 
-    # if params[:count]
-    #   # TODO: Move this to model
-    #   @works_hash.each do |category, contents|
-    #     @works_hash[category] = @works_hash[category][contents].sample(params[:count])
-    #   end
-    # end
-
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,9 @@
 class Work < ApplicationRecord
+  def top_ten(category)
+    return Work.where(category: category).sample(10)
+  end
+
+  def media_spotlight
+    return Work.all.sample
+  end
 end

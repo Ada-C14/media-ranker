f7bbae6d6668ae26035bb9a899a1f15aa215aabb
Alice D <alicedilanchian@gmail.com>
2020-11-12 12:11:41 -0500

    users_controller_test.rb updated; test_helper.rb updated

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,5 +17,18 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  def login(username = "Username 1")
+    user_hash = {
+        user: {
+            username: "Username 1"
+        }
+    }
+
+    post login_path, params: user_hash
+
+    user = User.find_by(username: username)
+    return user
+  end
+
   # Add more helper methods to be used by all tests here...
 end

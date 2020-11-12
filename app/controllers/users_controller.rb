c5baf06804de4f8bc0c6e35f076bac9a45f54bfb
Anna Kim <anna.kim93@gmail.com>
2020-11-12 17:21:21 -0500

    added a login action which logs in exisitng user or creates a new user based on username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,34 @@
 class UsersController < ApplicationController
+  # Helper Methods
+  def successful_login(type, user)
+    flash[:notice] =
+      if type == 'new'
+        "Successfully created new user #{user.username} with ID #{user.id}"
+      else
+        "Successfully logged in as existing user #{user.username}"
+      end
+  end
+
+  #########################################################
+
   def login_form
     @user = User.new
   end
 
   def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
 
+    if user
+      session[:user_id] = user.id
+      successful_login('existing', user)
+    else
+      user = User.create(username: username)
+      session[:user_id] = user.id
+      successful_login('new', user)
+    end
+
+    redirect_to root_path
+    return
   end
 end

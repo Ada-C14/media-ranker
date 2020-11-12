6dc4e80957f594a433f833f497f22aabd4436238
Genevieve Neely <genevieve.hood@gmail.com>
2020-11-11 20:36:46 -0800

    add user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'validations' do
+    before do
+      @user = User.new(
+          username: "Test user",
+      )
+    end
+
+    it 'is valid when the required fields are present' do
+      result = @user.valid?
+      expect(result).must_equal true
+    end
+
+    it 'is invalid without a username' do
+      @user.username = nil
+
+      result = @user.valid?
+      expect(result).must_equal false
+    end
+  end
+
+  describe 'relations' do
+    it 'can have many Votes' do
+      work = works(:book)
+
+      work.votes.each do |vote|
+        expect(vote).must_be_instance_of Vote
+      end
+      expect(work.votes.count).must_equal 3
+
+    end
+  end
 end

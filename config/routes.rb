e37ec3b639b4037ebc7347bfcb389b7c55d986e2
Emily Nagai <emnagai89@gmail.com>
2020-11-11 19:56:31 -0800

    added routes for root path, index, and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'works#index'
+  get '/works', to: 'works#index', as: 'works'
+  get '/works/:id', to: 'works#show', as: 'work'
+
 end

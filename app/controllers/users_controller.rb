1f5869cab9edbe9d127030b731b65b05f9a8376f
Li Dai <li.lea.dai@gmail.com>
2020-11-11 20:17:43 -0800

    added user actions and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-
   def show
+    @user = User.find_by(id: params[:id])
     if @user.nil?
       head :not_found
       return

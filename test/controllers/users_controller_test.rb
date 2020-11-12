b84b9c595b6d6bdff54b76ee6bfd4347164ec186
Li Dai <li.lea.dai@gmail.com>
2020-11-11 20:01:29 -0800

    added user controllers methods and tests to add session

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
+
+  # describe "current" do
+  #   it "returns 200 OK for a logged-in user" do
+  #
+  #     # Arrange
+  #     perform_login
+  #
+  #     # Act
+  #     get current_user_path
+  #
+  #     # Assert
+  #     must_respond_with :success
+  #   end
   # end
 end

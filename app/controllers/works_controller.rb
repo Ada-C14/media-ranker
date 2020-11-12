c859251ededb19a6669462b14216ccb4763bd049
beauttie <bak02013@pomona.edu>
2020-11-11 23:00:05 -0800

    Seed data, create controllers for Works and Users, define CR actions, create views for index and show

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,39 @@
+class WorksController < ApplicationController
+  def index
+    @works = Work.all
+  end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      redirect_to works_path
+      return
+    end
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to work_path(@work.id)
+      return
+    else
+      render :new, status: :bad_request
+      return
+    end
+  end
+
+  def edit
+
+  end
+
+  private
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+end

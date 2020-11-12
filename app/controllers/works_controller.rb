8a6a1fc1b87700936b4b1dbe2458d11bcd234073
Marj E <marj@marjorieetienne.com>
2020-11-12 12:11:23 -0800

    added method holders in work controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,28 @@ class WorksController < ApplicationController
   def index
     @works = Works.all
   end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end

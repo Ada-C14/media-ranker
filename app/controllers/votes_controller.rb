e4c388391b0639c4c197f3b79c8dddd7f0054640
Jasmine <jasylop@gmail.com>
2020-11-12 07:48:46 -0800

    added create action

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,13 @@
 class VotesController < ApplicationController
+
+  def create
+    @vote = Vote.new(vote_params)
+    if @vote.save
+      flash[:success] = "Successfully upvoted!"
+      return
+    else
+      flash[:error] = "Something happened, vote not saved"
+      return
+    end
+  end
 end

ac9576d7cca08aca9c448232e1f64c9143388714
Ren Carothers <rencarothers@gmail.com>
2020-11-11 16:39:59 -0800

    updates index view to sort list by category using .by_category method; adds test reminder

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,6 @@
 class Work < ApplicationRecord
+
+  def self.by_category(category)
+    self.where(category: category).order(category: :desc) #TODO: change order to vote count when initialized
+  end
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
+  it "stuff about validations when those are implemented" do
   #   value(1+1).must_equal 2
-  # end
+  end
+
+  it "does stuff with the self.by_category method" do
+
+  end
 end

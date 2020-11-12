f36389b5c8c2949ad075bc609e3ba0688c48d33e
Iris Lux <iris.lux0@gmail.com>
2020-11-12 10:56:30 -0800

    moved top_ten and top_one to model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,14 @@
 class Work < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
+
+  def self.top_ten(category)
+    return Work.all.where(category: category).max_by(10){|work| work.votes.count}
+  end
+
+  def self.top_one
+    return Work.all.max_by{|work| work.votes.count}
+  end
+
+
 end

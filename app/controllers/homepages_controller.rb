182be5656a80f79551fbc1d3166c98415f8b1c87
Pauline Chane <pauline.chane@gmail.com>
2020-11-11 22:07:49 -0800

    can now add new works

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,6 +1,8 @@
 class HomepagesController < ApplicationController
   def index
-    @spotlight = Work.first
+    # when switching to sort, make sure to set it up so that there is no need to
+    # check for total #num of works in spotlight, try .limit(10)
+    @spotlight = Work.all.sample
     @books = Work.where(category: 'book').sample(10)
     @movies = Work.where(category: 'movie').sample(10)
     @albums = Work.where(category: 'album').sample(10)
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -16,23 +16,57 @@ class WorksController < ApplicationController
   end
 
   def new
-
+    @work = Work.new
   end
 
   def create
-
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to work_path(@work.id)
+      return
+    else
+      render :new
+      return
+    end
   end
 
   def edit
-
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    end
   end
 
   def update
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
 
+    if @work.nil?
+      head :not_found
+      return
+    end
+
+    if @work.update(work_params)
+      redirect_to work_path(@work.id)
+      return
+    else
+      render :edit
+      return
+    end
   end
 
   def destroy
-
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
+    # NOTE: confirmation page is handled by index and show pages as a dialog box.
+    if @work
+      @work.destroy
+      redirect_to works_path
+    else
+      head :not_found
+      return
+    end
   end
 
   private

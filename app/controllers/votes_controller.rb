d5bf760c1bf178be6be3f4f4116245d16a5ef416
Jasmine <jasylop@gmail.com>
2020-11-12 15:52:41 -0800

    adjusted validations and updated create method in votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,13 +1,25 @@
 class VotesController < ApplicationController
 
   def create
-    @vote = Vote.new(vote_params)
-    if @vote.save
-      flash[:success] = "Successfully upvoted!"
-      return
-    else
-      flash[:error] = "Something happened, vote not saved"
+    if session[:user_id].nil?
+      flash.now[:error] = "You need to be logged in to do that"
       return
     end
+
+    @vote = Vote.connect_work_user(params[:work_id], session[:user_id])
+
+    if @vote.save
+      flash[:success] = "Successfully upvoted!"
+      redirect_back fallback_location: root_path
+      return
+    else
+      # flash[:error] = "A problem occured: Could not upvote"
+      redirect_back fallback_location: root_path, status: :bad_request
+      return
+    end
+  end
+
+  def request_controller
+    params[:controller]
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,11 @@
 class Vote < ApplicationRecord
-  validates :votes, uniqueness: {scope: [:work_id, :user_id]}
+  validates :work_id, uniqueness: {scope: :user_id}
+
   belongs_to :work
   belongs_to :user
+
+  def self.connect_work_user(work_id, user_id)
+    return Vote.new(work_id: work_id, user_id: user_id)
+  end
 end
+

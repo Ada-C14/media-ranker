64f565459c0a3d50959dfdfba5c0f70494409ff1
Jasmine <jasylop@gmail.com>
2020-11-11 20:59:44 -0800

    renamed name to username in user column

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_041726) do
+ActiveRecord::Schema.define(version: 2020_11_12_045511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.date "date_joined"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

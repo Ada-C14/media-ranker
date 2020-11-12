b1ea9440e6db0dba896cffde1e502004c3c0fd73
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-11 18:10:42 -0800

    add RESTful routes and define works#show and works#index

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,34 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all
+  end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      flash.now[:error] = "Work not found"
+      redirect_to works_path and return
+    end
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end

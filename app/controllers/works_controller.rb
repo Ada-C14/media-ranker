6cdd24b1efb237806c6f4fc8b1069e716e2089d9
Mackenzie <mvlofthus@gmail.com>
2020-11-11 18:52:19 -0800

    work: edit and update, destroy, started some button CSS

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -28,6 +28,45 @@ class WorksController < ApplicationController
     end
   end
 
+  def edit
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      redirect_to works_path
+      return
+    end
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+      return
+    elsif @work.update(work_params)
+      redirect_to work_path(@work.id)
+      return
+    else # save failed :(
+    render :edit, status: :bad_request
+    return
+    end
+  end
+
+  def destroy
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      redirect_to works_path, head: :temporary_redirect
+      return
+    end
+
+
+    @work.destroy
+
+    redirect_to works_path
+    return
+  end
+
   def homepage
     @works = Work.all
     @spotlight = Work.all.sample(1)[0]

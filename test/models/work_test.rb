c2af987d87cb78e18029af02f737beeb46441c88
MonaRahmani <monarahmanimehr@gmail.com>
2020-11-11 22:39:22 -0800

    Fixtures created

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -29,4 +29,13 @@ describe Work do
     expect(result).must_equal false
     expect(@work.errors.messages).must_include :title
   end
+  it "is invalid with a non-unique title" do
+    # do we need the other fields? I'm going with no
+    unique_work = Work.create!(title: new_work.title)
+
+    expect(new_work.valid?).must_equal false
+    expect(new_work.errors.messages).must_include :title
+    expect(new_work.errors.messages[:title].include?("has already been taken")).must_equal true
+
+  end
 end

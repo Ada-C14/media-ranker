c11c4cab8bec813677f4eecb2ce0eccb72d1219d
Jasmine <jasylop@gmail.com>
2020-11-11 16:12:14 -0800

    added test to check that homepage works regardless of works

--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -2,7 +2,14 @@ require "test_helper"
 
 describe HomepagesController do
   describe "index" do
-    it "successfully navigates to index page" do
+    it "successfully navigates to root_path" do
+      get root_path
+      must_respond_with :success
+    end
+
+    it "successfully loads root path even if there are no works" do
+      works(:hp1).delete
+      expect(Work.count).must_equal 0
       get root_path
       must_respond_with :success
     end

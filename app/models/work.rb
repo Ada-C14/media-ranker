895c9b8cfdb97bc1be1f858528d60b742c9abd4e
Schanen Ryan <schanen.r@gmail.com>
2020-11-11 19:18:58 -0800

    MODELS: Added relationships to vote, user and work models.

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,7 @@
 class Work < ApplicationRecord
+
+  has_many :votes
+  has_many :users, through: :votes
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,36 @@
 Rails.application.routes.draw do
-  get 'votes/index'
-  get 'votes/show'
-  get 'votes/new'
-  get 'votes/create'
-  get 'votes/edit'
-  get 'votes/update'
-  get 'votes/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
-  get 'works/index'
-  get 'works/show'
-  get 'works/new'
-  get 'works/create'
-  get 'works/edit'
-  get 'works/update'
-  get 'works/destroy'
+
+  root to: 'works#index'
+
+  resources :votes
+
+  resources :users do
+    resources :votes
+  end
+
+  resources :works do
+    resources :votes
+  end
+  # get 'votes/index'
+  # get 'votes/show'
+  # get 'votes/new'
+  # get 'votes/create'
+  # get 'votes/edit'
+  # get 'votes/update'
+  # get 'votes/destroy'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/create'
+  # get 'users/edit'
+  # get 'users/update'
+  # get 'users/destroy'
+  # get 'works/index'
+  # get 'works/show'
+  # get 'works/new'
+  # get 'works/create'
+  # get 'works/edit'
+  # get 'works/update'
+  # get 'works/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

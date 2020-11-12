2fa55ce45603f42a5e4c822288e96ded340b706a
Genevieve Neely <genevieve.hood@gmail.com>
2020-11-11 22:10:03 -0800

    remove unneeded route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'works#main'
   resources :works
-  get 'works/main', to: "works#main"
 
   resources :users, only: [:index, :show]
   get "/login", to: "users#login_form", as: "login"

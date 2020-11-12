679f7a164b84c0009d9e72364348a4ffcd1e1f3f
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-11 18:59:07 -0800

    add validation tests for work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,6 @@
 class Work < ApplicationRecord
   validates :category, inclusion: { in: %w(album book movie)}
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :publication_year, numericality: { only_integer: true }, allow_nil: true
 
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,52 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
+  describe "validations" do
+    before do
+      @work = Work.new(category: "book", title: "Death on the Nile", creator: "Agatha Christie", publication_year: 1937, description: "Hercule Poirot is not allowed to take a vacation")
+    end
+
+    it "creates a work when given valid data" do
+      success = @work.valid?
+
+      expect(success).must_equal true
+    end
+
+    it "won't create a work without a title" do
+      @work.title = nil
+
+      success = @work.valid?
+
+      expect(success).must_equal false
+    end
+
+    it "won't create a work with a duplicate title" do
+      @work.save
+      @second_work = Work.new(category: "album", title: "Death on the Nile")
+
+      success = @second_work.valid?
+
+      expect(success).must_equal false
+    end
+
+    it "only accepts integers for publication years" do
+      @work.publication_year = "The Thirties"
+
+      success = @work.valid?
+
+      expect(success).must_equal false
+    end
+
+    it "will not create a work with an invalid category" do
+      @work.category = "TV Show"
+
+      success = @work.valid?
+
+      expect(success).must_equal false
+    end
+  end
+
+  # describe "relationships" do
+  #
   # end
 end

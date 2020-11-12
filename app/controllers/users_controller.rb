bf08ae82fd1ac4d7716a44fee4af71f9c4b35402
Kareha Agesa <kmaagesa@gmail.com>
2020-11-11 16:42:06 -0800

    logout and logged in as buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,6 @@ class UsersController < ApplicationController
   end
 
   def logout
-    raise
     session[:user_id] = nil
     flash[:success] = "Successfully logged out"
     redirect_to root_path

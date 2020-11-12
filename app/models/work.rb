148e02aae660ea7b137b5845fa2a527f23de9f29
Ren Carothers <rencarothers@gmail.com>
2020-11-11 17:55:34 -0800

    adds homepage controller; methods to display top_ten and spotlight; index view

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,4 +3,13 @@ class Work < ApplicationRecord
   def self.by_category(category)
     self.where(category: category).order(category: :desc) #TODO: change order to vote count when initialized
   end
+
+  def self.media_spotlight
+    return Work.order('RANDOM()').first
+  end
+
+  def self.top_ten(category)
+    return Work.by_category(category).order('RANDOM()').limit(10)
+  end
 end
+

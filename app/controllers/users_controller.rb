76d1465835db7684d6410ab169c194bff48b593f
Anna Kim <anna.kim93@gmail.com>
2020-11-12 19:20:53 -0500

    add logout action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,18 @@ class UsersController < ApplicationController
     return
   end
 
+  def logout
+    if session[:user_id]
+      user = User.find_by(id: session[:user_id])
+      user ? flash[:success] = 'Successfully logged out' : flash[:notice] = 'Error: unknown user'
+      session[:user_id] = nil
+    else
+      flash[:notice] = "Must be logged in first to log out"
+    end
+
+    redirect_to root_path
+  end
+
   private
 
   def user_params

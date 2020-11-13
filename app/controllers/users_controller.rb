f363a655a89348a4197062c184097248633bc2b9
Emily Nagai <emnagai89@gmail.com>
2020-11-12 22:45:05 -0800

    implemented users index and show actions. updated routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
 
+  def show
+    @user = User.find_by(id: params[:id])
+
+    if @user.nil?
+      redirect_to root_path
+      return
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'works#index'
   resources :works
-  # get '/works', to: 'works#index', as: 'works'
-  # get '/works/new', to: 'works#new', as: 'new_work'
-  # post '/works', to: 'works#create'
-  #
-  # get '/works/:id', to: 'works#show', as: 'work'
-  # patch '/works/:id/edit', to: 'works#edit', as: 'edit_work'
-  # get '/works/:id', to: 'works#update'
-  # delete '/works/:id', to: 'works#destroy'
+  resources :users, only: [:index, :show]
 
 end

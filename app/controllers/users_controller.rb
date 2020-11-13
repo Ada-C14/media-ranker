ff8b50d9a1958d7943d960c35cd1db1c5c7bf3c8
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-13 01:52:57 -0800

    add user login feature (couldn't read current user yet)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,23 +11,45 @@ class UsersController < ApplicationController
     end
   end
   
-  def new
+  def login_form
     @user = User.new
   end
 
-  def create
+  def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as existing user #{ username }"
+      redirect_to root_path
+      return
+    end
+
     @user = User.new(user_params)
-    
-    # if username is existed, flash "Successfully logged in as existing user Cinnamon Toast" and redirect to root_path
     if @user.save
-      flash[:success] = "Successfully created new user #{ params[:username] } with ID #{ params[:id] }"
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully created new user #{ @user.username } with ID #{ @user.id }"
       redirect_to root_path
       return
     else
-      # if no username is provided + errors.message on view
-      # title is sensitive to up/lowercase, but up/lowercase are two different users. 
       flash.now[:error] = "A problem occurred: Could not log in"
-      render :new, status: :bad_request 
+      render :login_form, status: :bad_request 
+      return
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
+    return
+  end
+
+  def current
+    @current_user = User.find_by(id: session[:user_id])
+    unless @current_user
+      flash[:error] = "A problem occured: You must log in to do that"
+      redirect_to root_path
       return
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'homepages#index'
   resources :homepages, only: [:index] 
-  resources :users, only: [:index, :show, :new, :create]
+
+  resources :users, only: [:index, :show]
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"

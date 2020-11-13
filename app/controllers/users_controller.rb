24cd0029839e3a43371ad8fd4b4a0340f15511d1
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-13 15:30:38 -0800

    Updated login controller for user and can now log into website

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,22 @@ class UsersController < ApplicationController
   end
 
   def login
-
+    user = User.find_by(username: params[:user][:username])
+    if user.nil?
+      # New User
+      user = User.new(username: params[:user][:username])
+      if ! user.save
+        flash[:error] = "Unable to login"
+        redirect_to homepage_path
+        return
+      end
+      flash[:welcome] = "Successfully created new #{user.username} with ID #{user.id}"
+    # Existing User
+  else
+    flash[:welcome] = "Successfully logged in as existing user #{user.username}"
+    end
+    session[:user_id] = user.id
+    redirect_to homepage_path
   end
 
   def logout

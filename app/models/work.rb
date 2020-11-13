8dbe1a9a1a680219dec0fca16df9cd5a48e23f92
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-12 23:41:25 -0800

    Created yaml file and started testing work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,7 @@
 class Work < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
+  validates :title, presence: true, uniqueness: true
 
 
   def spotlight
@@ -8,13 +9,6 @@ class Work < ApplicationRecord
   end
 
 
-  def self.sort_category(category)
-    solo_category = Work.where(category: category)
-   solo_category.each do |category|
-     category.title
-     category.creator
-     end
-   end
 end
 
 
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  describe 'validations' do
+    it "is valid when all field are present" do
+      #Arrange
+      work = works(:working_media)
+
+      #Act
+      result = work.valid?
+
+      #Assert
+      expect(result).must_equal true
+    end
+
+    it "is invalid without a title" do
+      #Arrange
+      work = works(:bad_media)
+
+      #Act
+      result = work.valid?
+
+      #Assert
+      expect(work.valid?).must_equal false
+    end
+
+
+  end
+
+
 end

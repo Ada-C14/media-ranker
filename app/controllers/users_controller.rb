22029e0fd8e42ef2b4dea5899d83539900cf4d17
Yingjing Lin <yjlin789@gmail.com>
2020-11-12 22:26:29 -0800

    add user model and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def login_form
+
+
+  end
+
+
+  def login
+
+  end
+
+  def logout
+
+  end
+
+  def current
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root to: 'homepages#index'
 
   resources :works
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "user#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end

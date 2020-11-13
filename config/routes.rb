902117afc6b62cdbd40a48359c769375c9e56d4d
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-13 09:10:40 -0800

    Created new user routes and began testing user controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   resources :works
   resources :homepages
-  resources :users
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "users/current", to: "users#current", as: "current_user"
 
 
 

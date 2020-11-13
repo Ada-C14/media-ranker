7345d75adaa0d4d2330ccbc4c63e466af385ec25
Iris Lux <iris.lux0@gmail.com>
2020-11-12 18:29:30 -0800

    user controller - login, login_form, logout, current - assoc. routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  def login_form
+    @user = User.new
+  end
+
+  def login
+    name = params[:user][:name]
+    user = User.find_by(name: name)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as returning user #{name}"
+    else
+      user = User.create(name: name)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as new user #{name}"
+    end
+
+    redirect_to root_path
+    return
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
+    return
+  end
+
+  def current
+    @current_user = User.find_by(id: session[:user_id])
+    unless @current_user
+      flash[:error] = "You must be logged in to see this page"
+      redirect_to root_path
+      return
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
+
   resources :works
   resources :homepages, only: [:index]
+
   root to: 'homepages#index'
 
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end

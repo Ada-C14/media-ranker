2a57307505a39ba70e3635d4e979abda688a915a
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-13 09:28:19 -0800

    Created login form and restyled homepage login button and added new login_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,20 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.all
-  end
-
-  def show
-  @user = User.find_by(id: params[:id])
-  if @user.nil?
-    head :not_found
-    return
-  end
-  end
-
-  def new
+  def login_form
     @user = User.new
+  end
+
+  def login
+
+  end
+
+  def logout
+
   end
 
 private
 
 def user_params
-  return params.require(:user).permit(:name)
+  return params.require(:user).permit(:username)
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :works
   resources :homepages
+  resources :users
 
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"

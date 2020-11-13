f90f9eb09cac2e6e9eefffeb5f96ebbfe70db93a
Jasmine <jasylop@gmail.com>
2020-11-12 20:01:58 -0800

    added in all the model tests for vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,9 @@ class Vote < ApplicationRecord
   validates :work_id, uniqueness: {scope: :user_id}
 
   belongs_to :work
+
   belongs_to :user
+
 
   def self.connect_work_user(work_id, user_id)
     return Vote.new(work_id: work_id, user_id: user_id)
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,53 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    it "it is valid with all fields" do
+      work = works(:hp1)
+      user = User.create!(username: "Jasmine")
+      vote = Vote.new(work_id: work.id, user_id: user.id)
+      expect(vote.valid?).must_equal true
+
+    end
+
+    it "is invalid without work_id" do
+      vote = Vote.new(work_id: works(:hp1).id)
+      expect(vote.valid?).must_equal false
+    end
+
+    it "is invalid without user_id" do
+      vote = Vote.new(user_id: users(:harry).id)
+      expect(vote.valid?).must_equal false
+    end
+
+    it "it is invalid if combo of work_id and user_id are not unique" do
+      work = works(:hp1)
+      user = users(:harry)
+      vote = votes(:vote_1) # is linked to work and user in fixture
+      vote_copy = Vote.new(work_id: work.id, user_id: user.id)
+      expect(vote_copy.valid?).must_equal false
+    end
+
+  end
+  describe "custom methods" do
+    describe "connect work and user" do
+      it "returns vote object given a work_id and user_id" do
+        work_id = works(:hp1).id
+        user_id = User.new(username: "Hermione").id
+        expect(Vote.connect_work_user(work_id, user_id)).must_be_instance_of Vote
+      end
+    end
+
+  end
+  describe "relationships" do
+    it "belongs to a user" do
+      vote = votes(:vote_1)
+      expect(vote.user).must_be_instance_of User
+    end
+
+    it "it belongs to a work" do
+      vote = votes(:vote_2)
+      expect(vote.work).must_be_instance_of Work
+    end
+  end
 end

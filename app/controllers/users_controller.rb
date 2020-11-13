bc653d0cd8440a7c582754774d00d881bbc7c961
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-12 23:11:09 -0800

    Began working on user pages and functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  @user = User.find_by(id: params[:id])
+  if @user.nil?
+    head :not_found
+    return
+  end
+  end
+
+  def new
+    @user = User.new
+  end
+
+private
+
+def user_params
+  return params.require(:user).permit(:name)
+end
 end

e00df0f7e3f9e7892469ea01735cc5e03e234a2a
Lina Do <l.do5147@gmail.com>
2020-11-12 22:56:52 -0800

    added routes for users, login/logout function

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   resources :works
 
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+
 end

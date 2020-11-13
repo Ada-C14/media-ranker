3c3f32b06d703036c589162227132ebebe08645e
Emily Nagai <emnagai89@gmail.com>
2020-11-12 22:51:57 -0800

    added user login/logout custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'works#index'
   resources :works
-  resources :users, only: [:index, :show]
 
+  get '/login', to: 'users#login_form', as: 'login'
+  post '/login', to: 'users#login'
+  post '/logout', to: 'users#logout', as: 'logout'
+  get '/users/current', to: 'users#current', as: 'current_user'
+  resources :users, only: [:index, :show]
 end

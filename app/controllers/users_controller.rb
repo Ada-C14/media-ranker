97486c8c27a329a808f3195e1957b87c4070c231
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-12 18:29:20 -0800

    define login_form, login, logout methods, update routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+  def login_form
+    @user = User.new
+  end
+
+  def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Welcome back, #{user.username}"
+    else
+      user = User.create(username: username)
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, new user #{user.username}"
+    end
+
+    redirect_to root_path and return
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "Goodbye, you are logged out"
+    redirect_to root_path and return
+  end
+
+  def current_user
+
+  end
 end

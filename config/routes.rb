decf10c153b8c13125fe6a9e063b1bde7a7bd0c8
Ren Carothers <rencarothers@gmail.com>
2020-11-13 10:04:06 -0800

    adds users controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :works
 
   resources :homepages, only: [:index]
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login" post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end

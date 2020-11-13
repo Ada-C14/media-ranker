5ae366a1bb46f724611f508eab9870740b365eac
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-13 11:47:07 -0800

    add validations for votes and write tests

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,8 @@
 class Vote < ApplicationRecord
   belongs_to :work
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :work_id, presence: true
+  validates_uniqueness_of :user_id, scope: :work_id
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,71 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relationships" do
+    it "something" do
+      raise NotImplementedError
+    end
+  end
+
+  describe "validations" do
+    before do
+      @user = User.first
+      @work = Work.first
+    end
+
+    it "creates a vote when given a user and work" do
+      vote = Vote.new(user_id: @user.id, work_id: @work.id)
+
+      success = vote.valid?
+
+      expect(success).must_equal true
+    end
+
+    it "won't create a vote without a user" do
+      vote = Vote.new(work_id: @work.id)
+
+      success = vote.valid?
+
+      expect(success).must_equal false
+    end
+
+    it "won't create a vote without a work" do
+      vote = Vote.new(user_id: @user.id)
+
+      success = vote.valid?
+
+      expect(success).must_equal false
+    end
+
+    it "won't create a vote if that user has already voted on that work" do
+      vote = Vote.new(user_id: @user.id, work_id: @work.id)
+      vote.save
+      second_vote = Vote.new(user_id: @user.id, work_id: @work.id)
+
+      success = second_vote.valid?
+
+      expect(success).must_equal false
+    end
+  end
+
+  describe "upvote" do
+    before do
+      @user = User.new(username: "ringolingo")
+    end
+
+    it "adds a vote to a work when a user is logged in" do
+      @user.save
+      work = Work.first
+
+      raise NotImplementedError
+    end
+
+    it "does not add a vote if there is no logged in user" do
+      raise NotImplementedError
+    end
+
+    it "does not add a vote to a work logged in user has already voted for" do
+      raise NotImplementedError
+    end
+  end
 end

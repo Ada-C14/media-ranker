e848b5fef1cdf81c3148169d47653ef11157522c
Beatrice T <be@trice.codes>
2020-11-12 20:26:53 -0800

    Fixed/added links to show pages and wrote works#show controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,6 @@
 class WorksController < ApplicationController
   def index
-    # Hoping for future expansion in media categories
+    # Planning for future expansion in media categories
     @CATEGORIES = ['album', 'movie', 'book']
     # Hash is category => array of category objects
     @works_hash = {}
@@ -8,5 +8,14 @@ class WorksController < ApplicationController
       @works_hash[category] = Work.where(category: category)
     end
 
+  end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      redirect_to works_path
+      return
+    end
   end
 end

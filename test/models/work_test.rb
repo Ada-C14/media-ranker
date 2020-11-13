172ad48d46124d50abb5a796a64e1f153c0dabe9
Taylor Tompkins <taylormililani@gmail.com>
2020-11-13 15:35:37 -0800

    outline for works model tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+
+  describe "validations" do
+    it "allows a new work to be created with all the proper fields" do
+
+    end
+
+    it "Won't allow a new work to be created without all required fields" do
+
+    end
+  end
+
+  describe "category_filter" do
+    it "will return just the works from a particular category" do
+      @works = Work.all
+
+      expect {
+        @works.category_filter("Book").category
+      }.must_equal "Book"
+
+    end
+  end
+
+  describe "spotlight" do
+    it "will give 10 random works" do
+
+    end
+  end
 end

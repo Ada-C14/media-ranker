c1d1bf5b2d82e7bb6bc14c8b42bc50b1834f2b28
Alice Boone <alicegbonel@gmail.com>
2020-11-13 10:48:59 -0800

    add users model, controller, tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+
   root to: 'homepages#home'
+
   resources :works
+
+  get '/login', to: 'users#login_form', as: 'login'
+  post '/login', to: 'users#login'
+  post '/logout', to: 'users#logout', as: 'logout'
+
+  get 'users/current', to: 'users#current', as: 'current_user'
+
+      resources :works do
+    resources :votes, only: [:create, :destroy]
+  end
+
+
 end

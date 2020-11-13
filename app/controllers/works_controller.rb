a418a2b14f8ce8e318fb3c7217445eeefd7cd3fe
Stacy <lundquist.stacy@gmail.com>
2020-11-12 21:07:29 -0800

    Works_controller && work.rb

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,44 @@
 class WorksController < ApplicationController
   def home
+    @works = Work.all
+    @all_works = Work.list_all_works
+    @top_ten = Work.top_ten(@all_works)
+    @spotlight = Work.spotlight(@top_ten)
+  end
+
+  def index
+    @works = Work.all
+  end
+
+  def new
+    @work = Work.new()
+  end
+
+  def show
+    if @work.nil?
+      flash.now[:failure] = "Not found! Try again?"
+      redirect_to work_path
+    end
+  end
+
+
+  private
+
+  def work_params
+    return params.require(:work).permit(
+        :category,
+        :title,
+        :creator,
+        :publication_year,
+        :description
+    )
+  end
+
+  def find_work
+    @work = Work.find_by(id: params[:id])
+  end
+
+  def find_work_votes
+    @work_votes = Work.find_by(id: params[:id]).votes
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,30 @@
 class Work < ApplicationRecord
+  has_many :votes
+  has_many :users, through: :votes
+
+  validates :title, presence: true, uniqueness: { scope: :category, case_sensitive: false },
+                    length: { maximum: 40 }
+  validates :category, presence: true, inclusion: { in: VALID_CATEGORIES,
+                                                    message: "%{value} is not a valid category" }
+  validates :creator, presence: true, length: { maximum: 30 }
+  validates :publication, presence: true,
+                          numericality: { only_integer: true,
+                                          less_than_or_equal_to: Date.today.year,
+                                          greater_than_or_equal_to: 1000 }
+  validates :description, length: { maximum: 140 }
+
+  # for WorksController#home
+  def self.list_all_works
+    all_works = Hash.new
+    VALID_CATEGORIES.each do |category|
+      works_by_category = Work.by_category(category)
+      all_works[category] = Work.sort(works_by_category)
+    end
+    return all_works
+  end
+
+  def self.by_category(category)
+    Work.where(category: category).order(title: :asc).to_a
+  end
+
 end

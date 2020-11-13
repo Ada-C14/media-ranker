3483c7505a34d4b64618e32d2d759dee46d7db65
Lina Do <l.do5147@gmail.com>
2020-11-12 23:11:28 -0800

    moved the login_form method from the model to the controller, incorrect placement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+
+  def login_form
+    @user = User.new
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ApplicationRecord
-  def login_form
-    @user = User.new
-  end
+
 end

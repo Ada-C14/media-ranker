a57d896bb8601383ce33f1bf6122c66aa6fc1be9
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-12 20:13:30 -0800

    write controller tests for User login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/login', to 'users#login_form', as: 'login'
-  post '/login', to 'users#login'
-  post '/logout', to 'users#logout', as: 'logout'
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
 
-  get '/users/current', to 'users#current', as: 'current_user'
+  get "/users/current", to: "users#current", as: "current_user"
 
-  root to: 'homepages#index'
+  root to: "homepages#index"
   resources :homepages, only:[:index]
   resources :works
 end

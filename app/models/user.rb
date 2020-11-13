b2726589a8e9ba99e771537045407b90ec76df16
Anna Kim <anna.kim93@gmail.com>
2020-11-13 00:05:08 -0500

    added user model validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :votes
   has_many :works, through: :votes
 
+  validates :username,
+            presence: true,
+            uniqueness: true
+
   def self.get_session_user(session_id)
     User.find_by(id: session_id)
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  let (:user) {
+    User.create(username: 'annakim')
+  }
+
+  it 'can be instantiated with the required fields' do
+    expect(user.valid?).must_equal true
+
+    user = User.first
+    expect(user).must_respond_to :username
+  end
+
+  describe 'relationships' do
+
+  end
+
+  describe 'validations' do
+    it 'must have a username' do
+      user.username = nil
+      expect(user.valid?).must_equal false
+      expect(user.errors.messages).must_include :username
+    end
+
+    it 'must have a unique username' do
+      user
+      user2 = User.create(username: 'annakim')
+
+      expect(user2.valid?).must_equal false
+      expect(user2.errors.messages).must_include :username
+      expect(user2.errors.messages[:username]).must_equal ['has already been taken']
+    end
+  end
+
+  describe 'custom methods' do
+
+  end
 end

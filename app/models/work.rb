791187d6eac2c790bb5bb7677e8ef522e03c34de
Emily Nagai <emnagai89@gmail.com>
2020-11-12 20:25:39 -0800

    refactored top ten method in work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,24 +1,14 @@
 class Work < ApplicationRecord
-  has_many :users, through: :votes
+  # has_many :users, through: :votes
+  validates :title, presence: true
+  validates :category, presence: true
 
   def self.select_spotlight
-    @spotlight = Work.all.sample.title
-    return @spotlight
+    spotlight = Work.all.sample
+    return spotlight
   end
 
   def self.top_ten(category)
-    @top_ten = Work.where(category).sample(10)
+    top_ten = Work.where(category: category).sample(10)
   end
-
-  # def find_top_books
-  #   return Work.where(category: 'book').sample(5)
-  # end
-  #
-  # def find_top_ten_albums
-  #   return Work.where(category: 'album').sample(5)
-  # end
-  #
-  # def find_top_movies
-  #   return Work.where(category: 'movies').sample(5)
-  # end
 end

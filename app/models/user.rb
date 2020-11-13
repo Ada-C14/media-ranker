e1971de5d2ca2224acd5cf2627f68cfb130b9e89
MonaRahmani <monarahmanimehr@gmail.com>
2020-11-12 20:05:41 -0800

    added relation and validates to the user and vote models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validation :username, presence:true, uniqueness: true
+
+  has_many :votes
+  has_many :works, through: :votes
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,8 @@
 class Vote < ApplicationRecord
+  belongs_to :work
+  belongs_to :user
+
+  # scope: allow just one user
+  validates :work_id, presence: true, uniqueness: { scope: user_id }
+  validates :user_id, presence: true
 end

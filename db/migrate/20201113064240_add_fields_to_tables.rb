49d6317124b15b7fb5ee24cabeb8f07d719cf2c3
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-13 12:35:26 -0800

    add migrations to relate votes to user and work

--- db/migrate/20201113064240_add_fields_to_tables.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldsToTables < ActiveRecord::Migration[6.0]
-  def change
-    add_column :votes, :user_id, :integer
-    add_column :votes, :work_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_064240) do
+ActiveRecord::Schema.define(version: 2020_11_13_203108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,10 @@ ActiveRecord::Schema.define(version: 2020_11_13_064240) do
   create_table "votes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.integer "work_id"
+    t.bigint "work_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
   create_table "works", force: :cascade do |t|

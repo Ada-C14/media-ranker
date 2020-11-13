f34f114d4808b2c015d47f6e94a3fc8d7d33e87d
marks214 <aimee.oz.x@gmail.com>
2020-11-12 17:17:02 -0800

    finished work model validations

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,6 @@
 class Work < ApplicationRecord
   has_many :votes
-  validates :category, presence: true#, inclusion: { in: Work.category }
+  validates :category, presence: true, inclusion: { within: %w[movie book album] }
   validates :title, presence: true
   validates :creator, presence: true
   validates :publication_year, presence: true
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,59 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    # something
+  end
+
+  it "can be instantiated" do
+    # test each entry from work.yml file:
+    works.each do |work|
+      expect(work.valid?).must_equal true
+    end
+  end
+
+  it "will must have the required fields" do
+    # each entry is failing a validation
+    invalid_works = [
+      test1_book = Work.new(
+        category: 'car', # invalid category
+        title: 'test book 1',
+        creator: 'test author 1',
+        publication_year: 2011,
+        description: 'test book description 1'
+      ),
+      test2_book = Work.new(
+        category: 'book',
+        # missing title
+        creator: 'test author 1',
+        publication_year: 2011,
+        description: 'test book description 1'
+      ),
+      test3_book = Work.new(
+        category: 'book',
+        title: 'test book 1',
+        # missing creator
+        publication_year: 2011,
+        description: 'test book description 1'
+      ),
+      test4_book = Work.new(
+        category: 'book',
+        title: 'test book 1',
+        creator: 'test author 1',
+        # missing publication year
+        description: 'test book description 1'
+      ),
+      test5_book = Work.new(
+        category: 'book',
+        title: 'test book 1',
+        creator: 'test author 1',
+        publication_year: 2011,
+        # missing description
+      )
+    ]
+    invalid_works.each do |invalid_work|
+      expect(invalid_work.valid?).must_equal false
+    end
+  end
+
 end

ba0bf2487ce28de5008c4ab84877cf18281696e1
Emily Nagai <emnagai89@gmail.com>
2020-11-12 23:22:09 -0800

    implemented user login_form and login actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,46 @@ class UsersController < ApplicationController
     end
   end
 
-  # def login_form
-  #
+  # def new
+  #   @user = User.new
   # end
+  #
+  # def create
+  #   @user = User.new(users_param)
+  #
+  #   if @user.save
+  #     redirect_to root_path
+  #     return
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  def login_form
+    @user = User.new
+  end
+
+  def login
+    @user = User.find_by(username: params[:user][:username])
+
+    if @user.nil?
+      @user = User.create(username: params[:user][:username])
+      unless @user.save
+        flash[:error] = "Unable to log in."
+        redirect_to root_path
+        return
+      end
+      flash[:welcome] = "Welcome #{@user.username}!"
+    else
+      flash[:welcome] = "Welcome back #{@user.username}"
+    end
+    session[:user_id] = @user.id
+    redirect_to root_path
+  end
+
+  private
+
+  def users_param
+    return params.require(:user).permit(:id, :username)
+  end
 end

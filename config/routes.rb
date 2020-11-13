1a4b3b82cf4f4920c6a3a8644550a1d587b80ab3
Beatrice T <be@trice.codes>
2020-11-13 12:21:18 -0800

    Wrote routes for user login and made Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
 
   resources :works
 
+  get '/login', to: 'users#login_form', as: 'login'
+  post '/login', to: 'users#login'
+  post '/logout', to: 'users#logout'
+  get '/users/current', to: 'users#current', as: 'current_user'
+  
   # get '/works/:count', to: 'works#index', as: :works_count
 end

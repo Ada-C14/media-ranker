38000204e8f42fa754a85f5a60f7c8b3f776ecfb
Beatrice T <be@trice.codes>
2020-11-12 20:17:30 -0800

    Wrote model tests for work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,11 @@
 class Work < ApplicationRecord
+  CATEGORIES = ['album', 'book', 'movie']
+  validates :title, presence: true, uniqueness: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES ,
+            message: "%{value} is not a valid category" }
+  validates :publication_year, numericality: { only_integer: true, allow_nil: true,
+                                               allow_blank: true}
+
   def self.top_ten(category)
     return Work.where(category: category).sample(10)
   end

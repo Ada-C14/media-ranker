499e22d397720efbc89aa0d113098a79d8a329d1
Lina Do <l.do5147@gmail.com>
2020-11-13 00:23:41 -0800

    changed login action to handle cases where the user is not found or if the username was left empty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,19 +6,22 @@ class UsersController < ApplicationController
 
   def login
     username = params[:user][:username]
-    user = User.find_by(username: username)
+    @user = User.find_by(username: username)
 
-    if user.nil?
-      flash.now[:error] = "A problem occurred: Could not log in"
-      render :login_form, status: :bad_request
-    elsif user
-      session[:user_id] = user.id
-      flash[:success] = "Successfully logged in as existing user #{username}"
+    if @user.nil?
+      @user = User.new(username: username)
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:success] = "Successfully created new user #{username} with ID #{@user.id}"
+        return
+      else
+        flash.now[:error] = "A problem occurred: Could not log in"
+        render :login_form, status: :bad_request
+        return
+      end
     else
-      # create new user if the username does not exist
-      user = User.create(username: username)
-      session[:user_id] = user.id
-      flash[:success] = "Successfully created new user #{username} with ID #{user.id}"
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully logged in as existing user #{username}"
     end
 
     redirect_to root_path

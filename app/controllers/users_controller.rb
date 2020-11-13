94bd175da38d26e8d91b0243d89f44de1e910806
Lina Do <l.do5147@gmail.com>
2020-11-12 23:24:03 -0800

    added login action method, existing or create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
   def login_form
     @user = User.new
   end
+
+  def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+
+    # existing user
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as existing user #{username}"
+    else
+      # create new user if the username does not exist
+      user = User.create(username: username)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully created new user #{username} with ID #{user.id}"
+    end
+
+    redirect_to root_path
+  end
 end

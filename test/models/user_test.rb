d1d108286da38561b20e2c69290ddd60ca0e4f18
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-12 18:05:01 -0800

    add and test validations for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    before do
+      @user = User.new(username: "ringolingo")
+    end
+
+    it "creates a user when given valid input" do
+      success = @user.valid?
+
+      expect(success).must_equal true
+    end
+
+    it "won't create a user without a username" do
+      @user.username = nil
+
+      success = @user.valid?
+
+      expect(success).must_equal false
+    end
+
+    it "won't create a user with a repeat name" do
+      @user.save
+      clone_user = User.new(username: "ringolingo")
+
+      success = clone_user.valid?
+
+      expect(success).must_equal false
+    end
+  end
 end

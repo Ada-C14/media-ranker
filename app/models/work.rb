366c42112629ce3751cf9362690d9a64a94f7a44
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-12 23:53:59 -0800

    Added validation to work title

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,7 +1,7 @@
 class Work < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: {message: "must be unique"}
 
 
   def spotlight

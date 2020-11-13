14c0b9f59b5e0f2b0103fd133221db93b53c3543
Ren Carothers <rencarothers@gmail.com>
2020-11-13 15:51:53 -0800

    adds some tests for login/logout feature

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,49 @@
 require "test_helper"
 
 describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  describe "logging in" do
+    before do
+      @user = users(:testuser)
+    end
+
+    it "can get the login form" do
+      get login_path
+
+      must_respond_with :success
+    end
+
+    it "can create session for existing user" do
+      perform_login(@user)
+      expect(session[:user_id]).must_equal @user.id
+    end
+
+    it "can create session for a new user" do
+
+      login_data = {
+          user: {
+              username: "new_user",
+          },
+      }
+
+      expect{post login_path, params: login_data}.must_differ "User.count", 1
+      expect(session[:user_id]).must_equal User.last.id
+    end
+  end
+
+  describe "logging out" do
+    before do
+      @user = users(:testuser)
+    end
+
+    it "can log a user out" do
+      perform_login
+      expect(session[:user_id]).wont_be_nil
+
+      post logout_path
+      expect(session[:user_id]).must_be_nil
+      must_respond_with :redirect
+    end
+  end
 end
+

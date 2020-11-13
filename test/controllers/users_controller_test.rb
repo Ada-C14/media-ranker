6656047bc993e800c14f814fa15a13892adbf523
Alice Boone <alicegbonel@gmail.com>
2020-11-13 14:11:08 -0800

    Add controller test to users login

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,34 @@
 require "test_helper"
 
 describe UsersController do
-  it "must get login_form" do
-    get users_login_form_url
+  it "can get the login_form" do
+    get login_path
+
     must_respond_with :success
   end
 
-  it "must get login" do
-    get users_login_url
-    must_respond_with :success
-  end
+  describe "loggin in" do
+    it "can login a new user" do
+      user = nil
+      expect{
+        user = login()
+      }.must_differ "User.count", 1
 
+      must_respond_with :redirect
+
+      expect(user).wont_be_nil
+      expect(session[:user_id]).must_equal user.id
+      expect(user.username).must_equal "Grace Hopper"
+    end
+
+    it "can login an existing user" do
+      user = User.create(username: "Ed Sheeran")
+
+      expect{
+        login(user.username)
+      }.wont_change "User.count"
+
+      expect(session[:user_id]).must_equal user.id
+    end
+  end
 end

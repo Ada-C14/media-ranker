5f7f8d333f58c14144b717de604b9c342cdb747f
marks214 <aimee.oz.x@gmail.com>
2020-11-12 20:28:06 -0800

    added user routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
 
   resources :works
 
+  resources :users, only: [:index, :show]
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+
 end

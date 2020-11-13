d3d02323e37aa7c4dfbb5c762c024e3a9d0a6e12
Christina Minh <christina.minh@gmail.com>
2020-11-13 15:27:05 -0800

    Added Login, Log out, and Logged in as username. Fixed route for logout

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   get '/login', to: 'users#login_form', as: 'login'
   post '/login', to: 'users#login'
-  post '/login', to: 'users#logout', as: 'logout'
+  post '/logout', to: 'users#logout', as: 'logout'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

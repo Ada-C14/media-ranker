9c66425dbaef4e1d98933cff5db7851acd412671
Jasmine <jasylop@gmail.com>
2020-11-12 18:51:14 -0800

    set up some it blocks for testing

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 describe VotesController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "create" do
+    it "does not create a vote if user is not logged in" do
+
+    end
+
+    it "creates a vote with logged in user" do
+
+    end
+
+    it "will not let a logged in user vote more than once on the same work" do
+
+    end
+  end
 end

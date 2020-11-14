712ff84f83d4296ca173bb55393b59f7ce8711fa
Lina Do <l.do5147@gmail.com>
2020-11-14 15:04:01 -0800

    wrote test to validate unique votes

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -4,4 +4,38 @@ describe Vote do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
+  before do
+
+    work = Work.create(
+        category: "book",
+        title: "test work",
+        creator: "test creator",
+        publication_year: 2003,
+        description: "blah blah"
+    )
+    user = User.create(
+        username: "lalala23"
+    )
+
+    @vote = Vote.new(user: user, work: work)
+  end
+
+  it 'is valid when user has not voted for the work before' do
+    result = @vote.valid?
+
+    expect(result).must_equal true
+  end
+
+  it 'is invalid when user has already voted for the work' do
+
+    existing_vote = Vote.create(
+        user: User.find_by(username: "lalala23"),
+        work: Work.find_by(title: "test work")
+    )
+    result = @vote.valid?
+
+    expect(result).must_equal false
+  end
+
+
 end

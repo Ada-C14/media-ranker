4fa94069ac36f254e29ff0047c4b4c773704c954
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-11-13 16:09:52 -0800

    updated the index view

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,11 +5,22 @@ class Work < ApplicationRecord
   has_many :votes
   # has_many :users, through: :votes -- which users voted for what?
 
-  def self.top_ten
-
-  end
-
   def self.spotlight
     works = Work.all
+    works.max_by { |work| work.votes.count }
   end
+
+  def self.top_works(media)
+    all_results = Work.where(media: media.to_s)
+    unless all_results == []
+      return all_results.max_by(10) { |work| work.votes.count }
+    else
+      return []
+    end
+  end
+
+  def vote_count
+    return self.votes.count
+  end
+
 end

866b15a3a26f39f4388f81b8bd2fd963589c6cd2
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-13 17:18:58 -0800

    add test for session for user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "current" do
+    it "returns 200 OK for a logged-in user" do
+      # Arrange
+      perform_login
+    
+      # Act
+      get user_path(session[:user_id])
+    
+      # Assert
+      must_respond_with :success
+    end    
+  end
 end

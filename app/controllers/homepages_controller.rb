56591657eb288c6562b5619af9ccc73fd7e0c033
Christina Minh <christina.minh@gmail.com>
2020-11-14 13:54:58 -0800

    Added top 10 sections for homepage

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -4,5 +4,9 @@ class HomepagesController < ApplicationController
 
     @spotlight = Work.spotlight
 
+    WORK_CATEGORIES.each do |category|
+      var_name = "@#{category}s"
+      instance_variable_set(var_name, Work.by_category(category))
+    end
   end
 end

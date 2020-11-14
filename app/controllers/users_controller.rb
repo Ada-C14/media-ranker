f100f9042151438a6e14690e6b12debba04b0460
Blaine <b.atsbeha@gmail.com>
2020-11-13 17:36:43 -0800

    created login routes,intro to sessions: method in user controller, and login_form view page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,4 +24,50 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def login_form
+    @user = User.new
+  end
+
+  def login
+    user = User.find_by(name: params[:user][:name])
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as returning user #{user.name}"
+    else
+      user = User.create(name: params[:user][:name])
+      if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as new user #{user.name}"
+    end
+    # if @user.nil?
+    #   user = User.new(name: params[:user][:name])
+    #   if ! user.save
+    #     flash[:error] = "Unable to Login"
+    #     redirect_to root_path
+    #   end
+    #   flash[:welcome] = "Welcome #{user.name}"
+    # else
+    #   flash[:welcome] = "Welcome back #{user.name}"
+    # end
+    session[:user_id] = user.id
+    redirect_to root_path
+  end
+end
+  # def logout
+  #   if session[:user_id]
+  #     user = User.find_by(id: session[:user_id])
+  #     unless user.nil?
+  #       session[:user_id] = nil
+  #       flash[:notice] = "Goodbye #{user.name}"
+  #     else
+  #       session[:user_id] = nil
+  #       flash[:notice] = "Error Unknown User"
+  #     end
+  #   else
+  #     session[:user_id] = nil?
+  #     flash[:error] = "You must be Logged In to Logout"
+  #   end
+  #   redirect_to root_path
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :works
   resources :users, only: [:index, :show, :new, :create]
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+
+  # post "/logout", to: "users#logout", as: "logout"
+  # get "/users/current", to: "users#current", as: "current_user"
 end

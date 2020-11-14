f9cdb51b389e1bb4789cd5421352747c5ca9c0af
Pauline Chane <pauline.chane@gmail.com>
2020-11-14 14:34:41 -0800

    user fixtures

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,17 +1,13 @@
 require "test_helper"
 
 describe User do
-  let (:new_user) {
-    User.new(username: "user")
-  }
   it "can be instantiated" do
     # Assert
-    expect(new_user.valid?).must_equal true
+    expect(users(:user).valid?).must_equal true
   end
 
   it "will have the required fields" do
     # Arrange
-    new_user.save
     user = User.first
     # Assert
     expect(user).must_respond_to :username
@@ -19,35 +15,30 @@ describe User do
 
   describe "relationships" do
     before do
-      new_user.save
+      @new_work = Work.create(category: "album", title: "The Album")
+      @new_work2 = Work.create(category: "album", title: "The Other Album")
+      @vote_1 = Vote.create(user_id: users(:user).id, work_id: @new_work.id)
+      @vote_2 = Vote.create(user_id: users(:user).id, work_id: @new_work2.id)
     end
     it "can have many votes" do
       # arrange
-      new_work = Work.create(category: "album", title: "The Album")
-      new_work2 = Work.create(category: "album", title: "The Other Album")
-      vote_1 = Vote.create(user_id: new_user.id, work_id: new_work.id)
-      vote_2 = Vote.create(user_id: new_user.id, work_id: new_work2.id)
+
       # Assert
-      expect(new_user.votes.count).must_equal 2
-      expect(vote_1.work).must_equal new_work
-      expect(vote_2.work).must_equal new_work2
-      new_user.votes.each do |vote|
+      expect(users(:user).votes.count).must_equal 2
+      expect(@vote_1.work).must_equal @new_work
+      expect(@vote_2.work).must_equal @new_work2
+      users(:user).votes.each do |vote|
         expect(vote).must_be_instance_of Vote
-        expect(vote.user_id).must_equal new_user.id
+        expect(vote.user_id).must_equal users(:user).id
       end
     end
     it "can get information of works the user voted for" do
-      # arrange
-      new_work = Work.create(category: "album", title: "The Album")
-      new_work2 = Work.create(category: "album", title: "The Other Album")
-      vote_1 = Vote.create(user_id: new_user.id, work_id: new_work.id)
-      vote_2 = Vote.create(user_id: new_user.id, work_id: new_work2.id)
       # Assert
-      expect(new_user.works.count).must_equal 2
+      expect(users(:user).works.count).must_equal 2
       # check info
-      expect(new_user.works.find_by(title: "The Album")).must_equal new_work
-      expect(new_user.works.find_by(title: "The Other Album")).must_equal new_work2
-      new_user.works.each do |work|
+      expect(users(:user).works.find_by(title: "The Album")).must_equal @new_work
+      expect(users(:user).works.find_by(title: "The Other Album")).must_equal @new_work2
+      users(:user).works.each do |work|
         expect(work).must_be_instance_of Work
       end
     end
@@ -57,12 +48,12 @@ describe User do
   describe "validations" do
     it "must have a username" do
       # Arrange
-      new_user.username = nil
+      users(:user).username = nil
 
       # Assert
-      expect(new_user.valid?).must_equal false
-      expect(new_user.errors.messages).must_include :username
-      expect(new_user.errors.messages[:username]).must_equal ["can't be blank"]
+      expect(users(:user).valid?).must_equal false
+      expect(users(:user).errors.messages).must_include :username
+      expect(users(:user).errors.messages[:username]).must_equal ["can't be blank"]
     end
   end
 end

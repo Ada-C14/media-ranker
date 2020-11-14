781bbf0b0e8f9bb0554879bb131d33a87975ecd4
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-13 17:17:34 -0800

    revise flash msg

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -19,13 +19,13 @@ class WorksController < ApplicationController
     @work = Work.new(work_params)
 
     if @work.save
-      flash[:success] = "Successfully created #{ params[:category] } #{ params[:id] }"
+      flash[:success] = "Successfully created #{ @work.category } #{ @work.id }"
       redirect_to work_path(@work)
       return
     else
       # if no title is provided + errors.message on view
       # title is sensitive to up/lowercase, but up/lowercase are two different works. Different category couldn't share the same title.
-      flash.now[:error] = "A problem occurred: Could not create #{ params[:category] }"
+      flash.now[:error] = "A problem occurred: Could not create #{ params[:work][:category] }"
       render :new, status: :bad_request 
       return
     end
@@ -46,12 +46,11 @@ class WorksController < ApplicationController
       render :file => "#{Rails.root}/public/404.html",  layout: false, status: :not_found
       return
     elsif @work.update(work_params)
-      flash[:success] = "Successfully updated #{ params[:category] } #{ params[:id] }"
+      flash[:success] = "Successfully updated #{ @work.category } #{ @work.id }"
       redirect_to work_path(@work.id) 
       return
     else  
-      # Plus errors.message on view  
-      flash.now[:error] = "A problem occurred: Could not update #{ params[:category] }"  
+      flash.now[:error] = "A problem occurred: Could not update #{ @work.category }"  
       render :edit, status: :bad_request 
       return 
     end
@@ -66,6 +65,7 @@ class WorksController < ApplicationController
     end
 
     @work.destroy
+    flash[:success] = "Successfully destroyed #{ @work.category } #{ @work.id }"
     redirect_to root_path
     return
   end
@@ -73,24 +73,37 @@ class WorksController < ApplicationController
   def upvote
     @work = Work.find_by(id: params[:id])
     if @work.nil? 
-      render :file => "#{Rails.root}/public/404.html",  layout: false, status: :not_found
+      render :file => "#{Rails.root}/public/404.html", layout: false, status: :not_found
       return
     end
 
     vote = @work.votes.new(user_id: session[:user_id], work_id: @work.id, date: Time.now.strftime("%b %d, %Y"))
-    if vote.save
+    
+    if only_vote_once(@work)
+      flash[:error] = "A problem occurred: Could not upvote"
+      redirect_to work_path(@work.id) 
+      return
+    elsif vote.save
       flash[:success] = "Successfully upvoted!"
       redirect_to work_path(@work.id) 
       return  
     else
-      flash[:error] = "A problem occurred: Could not upvote"
+      flash[:error] = "A problem occurred: You must log in to do that"
       redirect_to work_path(@work.id) 
       return
     end
   end
 
   private
+
   def work_params
     return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+
+  def only_vote_once(work)
+    Vote.where(work_id: work.id).each do |vote|
+      return true if vote.user_id == session[:user_id]
+    end
+    return false
   end
 end

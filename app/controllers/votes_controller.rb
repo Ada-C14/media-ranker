873fd55873216ce5dc774e401fc89725bf3a9586
Jasmine <jasylop@gmail.com>
2020-11-13 20:07:19 -0800

    reformatted flash errors to accomondate validation errors

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,20 +1,20 @@
 class VotesController < ApplicationController
 
-  def create
+  def upvote
     if session[:user_id].nil?
-      flash.now[:error] = "You need to be logged in to do that"
+      flash[:error] = ["A problem occurred: You must log in to do that"]
+      redirect_back fallback_location: root_path
       return
     end
-
     @vote = Vote.connect_work_user(params[:work_id], session[:user_id])
-
     if @vote.save
       flash[:success] = "Successfully upvoted!"
       redirect_back fallback_location: root_path
       return
     else
-      # flash[:error] = "A problem occured: Could not upvote"
-      redirect_back fallback_location: root_path, status: :bad_request
+      flash[:error] = ["A problem occurred: Could not upvote"]
+      flash[:error] << @vote.format_errors
+      redirect_back fallback_location: root_path
       return
     end
   end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ApplicationRecord
-  validates :work_id, uniqueness: {scope: :user_id}
+  validates :user_id, uniqueness: {scope: :work_id, message: "has already voted for this work"}
 
   belongs_to :work, counter_cache: true
 

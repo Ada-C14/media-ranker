006b5effcfff3e0ea05ee27d29efe4214df50cbf
Genevieve Neely <genevieve.hood@gmail.com>
2020-11-13 19:46:16 -0800

    add styles w bootstrap

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -7,13 +7,4 @@ class Vote < ApplicationRecord
 
   validates :user_id, uniqueness: { scope: :work_id, message: "User has already upvoted this work" }
 
-  def self.upvote
-    vote = Vote.new(work_id: params[:work][:id], user_id: session[:user_id])
-
-    if vote.save
-      return true
-    else
-      return false
-    end
-  end
 end

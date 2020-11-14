f00fd9b87af13fa161b19c181a5f8cc5ce506d3e
Lina Do <l.do5147@gmail.com>
2020-11-14 10:59:28 -0800

    working on vote#create action, user cannot vote unless logged in, shows flash error message

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,14 @@
 class VotesController < ApplicationController
 
   def create
-
     # work nested route
-    work =
+    work_id = params[:work_id]
+    @work = Work.find_by(id: work_id)
+
+    if session[:user_id].nil?
+      flash[:error] = "A problem occurred: You must log in to do that"
+      redirect_to works_path
+    end
 
   end
 

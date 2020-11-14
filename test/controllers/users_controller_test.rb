b970370d787535e18c26bbfa8a3db4ed56291ab0
Pauline Chane <pauline.chane@gmail.com>
2020-11-13 16:31:46 -0800

    completed working tests for user session functions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -25,7 +25,7 @@ describe UsersController do
   describe "show" do
     # Arrange
     before do
-      User.create(username: "MrSnoozeboi34") 
+      User.create(username: "MrSnoozeboi34")
     end
     it "responds with success when showing an existing valid work" do
       # Arrange
@@ -45,6 +45,74 @@ describe UsersController do
       # Assert
       must_respond_with :not_found
     end
+  end
+  describe "session functions " do
+    before do
+      # login data
+      @login_data = {user: {username: "test"}}
+
+      @null_data = {user: {username: nil}
+      }
+    end
+
+    describe "login_form" do
+      it "responds with success" do
+        # Act
+        get login_path
+
+        # Assert
+        must_respond_with :success
+      end
+    end
+
+    describe "login" do
+      it "creates a new user if logging in for the first time" do
+
+        expect{
+          post login_path, params: @login_data
+        }.must_change "User.count", 1
+
+        user = User.find_by(username: "test")
+
+        # verify user is in session
+        expect(session[:user_id]).must_equal user.id
+
+      end
+
+      it "logs in an existing user if username matches" do
+        User.create(username: "test")
+
+        expect{
+          post login_path, params: @login_data
+        }.wont_change "User.count"
+
+        user = User.find_by(username: "test")
+
+        # verify user is in session
+        expect(session[:user_id]).must_equal user.id
+      end
+
+      it "renders login page with bad request if blank is attempted" do
+         expect{
+         post login_path, params: @null_data
+         }.wont_change "User.count"
+
+        assert_response :bad_request
+      end
+    end
+
+    describe "logout" do
+      it "logs out a user currently in session" do
+        post login_path, params: @login_data
+
+        expect{
+          post logout_path, params: @login_data
+        }.wont_change "User.count"
+
+        assert_nil(session[:user_id])
+      end
+    end
+
   end
 
 end

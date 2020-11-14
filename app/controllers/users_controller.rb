d5f0143dba58327a084961b3a6c4a6b7d4e93188
Ren Carothers <rencarothers@gmail.com>
2020-11-13 20:21:27 -0800

    pulls flash changes from wave1-implementing-session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render_404 unless @user
+  end
+
   def login_form
     @user = User.new
   end
@@ -8,11 +17,11 @@ class UsersController < ApplicationController
     user = User.find_by(username: username)
     if user
       session[:user_id] = user.id
-      #TODO: Add flash here
+      flash[:message] = "Successfully logged in as existing user #{user.username}"
     else
       user = User.create(username: username)
       session[:user_id] = user.id
-      #TODO: Add flash here
+      flash[:message] = "Successfully created new user #{user.username} with #{user.id}"
     end
 
     redirect_to root_path
@@ -21,18 +30,8 @@ class UsersController < ApplicationController
 
   def logout
     session[:user_id] = nil
-    flash[:status] = :success
-    flash[:result_text] = "Successfully logged out"
+    flash[:message] = "Successfully logged out"
     redirect_to root_path
-  end
-
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find_by(id: params[:id])
-    render_404 unless @user
   end
 
 end

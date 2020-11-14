0fe2f67a45fb89720020883c35637db073dd1b14
Li Dai <li.lea.dai@gmail.com>
2020-11-13 21:33:30 -0800

    updated work model validation testing

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,30 +1,38 @@
 require "test_helper"
 
 describe Work do
-  let (:work){
-    Work.create!(
-        category: "album",
-        title: "test",
-        creator: "testor",
-        publication_year: 2020,
-        description: "testing")
-  }
+  before do
+    @work = works(:work1)
+  end
 
   describe "validations" do
+    it 'is valid when all fields are present' do
+      # Act
+      result = @work.valid?
+
+      # Assert
+      expect(result).must_equal true
+    end
+
     it "is invalid without a title" do
-      work.title = nil
-      result = work.valid?
+      @work.title = nil
+      result = @work.valid?
       expect(result).must_equal false
-      expect(work.errors.messages).must_include :title
+      expect(@work.errors.messages).must_include :title
     end
 
     it "is invalid with a repeating title and validation is not case sensitive" do
-      Work.create!(
-          category: "album",
-          title: "Testing")
-      work.title = "testing"
-      result = work.valid?
+      work2 = works(:work2)
+      work2.title = "work1"
+      result = work2.valid?
       expect(result).must_equal false
+    end
+
+    it "is invalid without a category" do
+      @work.category = nil
+      result = @work.valid?
+      expect(result).must_equal false
+      expect(@work.errors.messages).must_include :category
     end
 
   end

083d428d932d313da9025b7a6c60c6a851f5f963
Alice Boone <alicegbonel@gmail.com>
2020-11-14 12:03:19 -0800

    validations changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
+
+  has_many :votes
+  has_many :works, through: :votes
+
   def self.get_session_user(session_id)
     User.find_by(id: session_id)
   end
 
-  has_many :votes
-  has_many :works, through: :votes
+  def self.user_joined
+    User.all.sort_by { |user| [user.votes.count, user.created_at] }.reverse
+  end
+
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -3,24 +3,7 @@ class Work < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
 
-  validates :title, :creator, :description, presence: true
-  validates :category, presence: true, inclusion: { in: %w[movie book album],
-              message: 'Category must be a movie, book, or album.'}
-
-  validates :publication_year, presence: true,
-            inclusion: { in: 1800..Date.today.year, message: 'Please pick a work created from 1800 onward.'},
-            numericality: { only_integer: true, message: 'Please enter an year, from 1800 onward.'}
-
-  validates :category, :title, presence: true
-  validates :publication_year, presence: true,
-            inclusion: {
-                in: 1800..Date.today.year,
-                message:'Please pick a work created from 1800 onward.'
-            },
-            numericality: {
-                only_integer: true,
-                message: 'Please enter an integer value for year, from 1800 onward.'
-            }
+  validates :title, presence: true
 
   def self.albums
     where(category: "album")
@@ -41,4 +24,5 @@ class Work < ApplicationRecord
   def self.top_ten(category)
     Work.where(category: category).limit(10)
   end
+
 end

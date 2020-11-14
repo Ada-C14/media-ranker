3a4e2aa58bfe0a14c17b45218de008b2aa91c44c
Jasmine <jasylop@gmail.com>
2020-11-13 20:03:43 -0800

    fixed typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homepages#index'
   resources :works do
-    resources :votes, only: [:create]
+    post "/upvote", to: 'votes#upvote', as: "upvote"
   end
   resources :users, only: [:index, :show]
 

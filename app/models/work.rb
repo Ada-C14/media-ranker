649004b0b44934d5d56d7f40eff57a8f73df40fa
Li Dai <li.lea.dai@gmail.com>
2020-11-13 20:07:59 -0800

    refactored top ten media method in work.rb

--- app/models/work.rb
+++ app/models/work.rb
@@ -7,6 +7,11 @@ class Work < ApplicationRecord
   # validates :creator, presence: true
   # validates :publication_year, numericality: { only_integer: true }
   # validates :description, presence: true
+  #
+  def self.top_ten(category)
+    works = Work.all.where(category:category)
+    return works.max_by(10) { |work| work.votes.count }
+  end
 
   def self.spotlight
     return Work.all.max_by {|work| work.votes.count}

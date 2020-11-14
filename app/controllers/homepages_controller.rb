78eed225ed377cd2fd058ae494a32237fcc1bc4d
KayKay-git <kayalew@yahoo.com>
2020-11-14 00:44:20 -0500

    WORK/HOMEPAGE: Added spotlight and top ten method to model. Added top ten and spotlight work model methods to index.

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,8 +1,8 @@
 class HomepagesController < ApplicationController
   def index
-    @work = Work.all.sample
-    @albums = Work.where(category: "album").sample(10)
-    @books = Work.where(category: "book").sample(10)
-    @movies = Work.where(category: "movie").sample(10)
+    @work = Work.spotlight
+    @albums = Work.top_ten("album")
+    @books = Work.top_ten("book")
+    @movies = Work.top_ten("movie")
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -5,8 +5,11 @@ class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :category }
 
   def self.spotlight
+    all_works = self.all
+    all_works.max_by { |work| work.votes.size}
   end
 
   def self.top_ten(category)
+    Work.where(category: category).sort_by { |work| work.votes.size }.reverse.first(10)
   end
 end

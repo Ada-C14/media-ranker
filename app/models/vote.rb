890b4c2fe42b5a73a53e602683dfca83637acc6b
Ren Carothers <rencarothers@gmail.com>
2020-11-13 20:05:34 -0800

    adds validation for votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
 
-  #TODO: add validation for if user already voted on a work
+  validates :user, uniqueness: { scope: :work, message: "has already voted for this work" }
 end

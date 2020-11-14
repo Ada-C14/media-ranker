509d00468f2b97a80d49958c1c4a9000be9f65a6
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-14 15:33:37 -0800

    Added instance variables to hompeages controller so that I can use them everywhere

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,7 +1,10 @@
 class HomepagesController < ApplicationController
 
   def index
-
+     @work = Work.all
+     @movies = @work.where(category: "movie")
+     @books = @work.where(category: "book")
+     @albums = @work.where(category: "album")
   end
 
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,11 +2,22 @@ class VotesController < ApplicationController
 
   before_action :require_login, only [:new, :create]
 
-  def upvote
-    @work_id = params[:work_id]
-    vote = Vote.new
-    vote.work_id = @work_id
-    vote.user_id = @current_user.id
+  def index
+    @votes = Vote.all
+  end
+
+  def new
+    @vote = Vote.new
+  end
+
+  def create
+    work = Work.find_by(id: params[:work_id])
+    user = User.find_by(id: session[:user_id])
+
+    @upvote = Vote.new(work_id: @work_id, user_id@current_user.id)
+
+
+
   end
 
 

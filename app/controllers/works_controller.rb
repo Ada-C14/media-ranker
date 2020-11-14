73225be883e248927f07db5cc11dfc0e28b7a736
Marj E <marj@marjorieetienne.com>
2020-11-14 14:23:41 -0800

    added the new work link and view

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -9,46 +9,46 @@ class WorksController < ApplicationController
   end
 
   def show
-    # @work = Work.find_by(id: params[:id])
-    # return :not_found if @work.nil?
+    @work = Work.find_by(id: params[:id])
+    return :not_found if @work.nil?
   end
 
   def new
-    # @work = Work.new
+    @work = Work.new
   end
 
   def create
-    # work = Work.new(work_params)
-    # return work.save ? redirect_to work_path(work) : render :new, status: :bad_request
+    work = Work.new(work_params)
+    return work.save ? (redirect_to work_path(work)) : (render :new, status: :bad_request)
   end
 
   def edit
-    # @work = Work.find_by(id: params[:id])
-    # return redirect_to root_path if @work.nil?
+    @work = Work.find_by(id: params[:id])
+    return redirect_to root_path if @work.nil?
   end
 
   def update
-    # work = Work.find_by(id: params[:id])
-    # if work.nil?
-    #   head :not_found
-    #   return
-    # elsif work.update(work_params)
-    #   redirect_to work_path(work.id)
-    #   return
-    # else
-    #   redirect_to root_path
-    # end
+    work = Work.find_by(id: params[:id])
+    if work.nil?
+      head :not_found
+      return
+    elsif work.update(work_params)
+      redirect_to work_path(work.id)
+      return
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
-    # work = Work.find_by(id: params[:id])
-    # if work.nil?
-    #   head :not_found
-    #   return
-    # else
-    #   work.destroy
-    #   redirect_to drivers_path
-    # end
+    work = Work.find_by(id: params[:id])
+    if work.nil?
+      head :not_found
+      return
+    else
+      work.destroy
+      redirect_to drivers_path
+    end
   end
 
   private

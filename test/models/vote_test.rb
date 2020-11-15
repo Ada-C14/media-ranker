1c32d1095ef38420d296a586c0e6c227c9583897
Pauline Chane <pauline.chane@gmail.com>
2020-11-14 16:43:04 -0800

    trying this last test again

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,12 +1,8 @@
 require "test_helper"
 
 describe Vote do
-  let (:new_vote) {
-    Vote.create(user_id: users(:user).id,
-                work_id: works(:book).id)
-  }
   it "can be instantiated" do
-    expect(new_vote.valid?).must_equal true
+    expect(votes(:vote).valid?).must_equal true
   end
   it "will have the required fields" do
     # Arrange
@@ -20,23 +16,23 @@ describe Vote do
   describe 'relationships' do
     it "can get the information of the user it belongs to" do
       # confirms that object exists and fields match
-      expect(new_vote.user).must_equal users(:user)
-      expect(new_vote.user.username).must_equal users(:user).username
-      expect(new_vote.user_id).must_equal users(:user).id
+      expect(votes(:vote).user).must_equal users(:user)
+      expect(votes(:vote).user.username).must_equal users(:user).username
+      expect(votes(:vote).user_id).must_equal users(:user).id
     end
     it "can get the information of the work it belongs to" do
       # confirms that object exists and fields match
-      expect(new_vote.work).must_equal works(:book)
-      expect(new_vote.work.title).must_equal works(:book).title
-      expect(new_vote.work.category).must_equal works(:book).category
-      expect(new_vote.work_id).must_equal works(:book).id
+      expect(votes(:vote).work).must_equal works(:book)
+      expect(votes(:vote).work.title).must_equal works(:book).title
+      expect(votes(:vote).work.category).must_equal works(:book).category
+      expect(votes(:vote).work_id).must_equal works(:book).id
     end
   end
 
   describe 'validations' do
     it 'prevents multiple votes for a work by the same user' do
-      copy_vote = Vote.create(user_id: new_vote.user_id,
-                              work_id: new_vote.work_id)
+      copy_vote = Vote.create(user_id: votes(:vote).user_id,
+                              work_id: votes(:vote).work_id)
       expect(copy_vote.valid?).must_equal false
       expect(copy_vote.errors.messages).must_include :user
       expect(copy_vote.errors.messages[:user]).must_equal ['has already voted for this work']

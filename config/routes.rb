74c816a10e6b5b1182a21505ee41094b9b7167a2
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-11-14 16:39:27 -0800

    login, logout function added

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
+
   get "/users/current", to: "users#current", as: "current_user"
+  get "/users/:id", to: "users#show", as: "user"
 end

1b5f4fbc8fd46eaaeed44d0e199548d5ea81c258
Mackenzie <mvlofthus@gmail.com>
2020-11-14 17:27:44 -0800

    user model tests pass, issues with user controller tests.  will come back to if time, want to practice bootstrap first

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -4,4 +4,40 @@ describe VotesController do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
+
+  # describe "create" do
+  #   it "can create a new vote with valid information and redirect" do
+  #
+  #     work = works(:gods)
+  #     user = users(:ron)
+  #
+  #
+  #     expect {
+  #       post work_votes_path(work.id), params: { work_id: work.id, user_id: user.id }
+  #     }.must_change "work.votes.count", 1 #come back if time, this isn't changing for some reason, not logged in... but haven't been able to corectly perform login
+  #
+  #     new_vote = Vote.last
+  #     expect(work.votes.count).wont_be_nil
+  #     expect(work.users.count).wont_be_nil
+  #     expect(new_vote.work_id).must_equal work.id
+  #     expect(new_vote.user_id).must_equal user.id
+  #     expect{flash[:success]}.must_equal "Successfully upvoted!"
+  #
+  #     expect(new_vote.driver.available).must_equal false
+  #
+  #     must_respond_with :redirect
+  #   end
+  #
+  #   it "won't create an invalid vote if user already voted on work" do
+      # driver.available = false
+      # driver.save
+      #
+      # # Act-Assert
+      # expect {
+      #   post passenger_trips_path(passenger.id)
+      # }.wont_change "Trip.count"
+      #
+      # must_redirect
+  #   end
+  # end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,40 @@ describe User do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
+  #
+
+  it "can be instantiated" do
+    # Assert
+    #
+    user1 = User.create(name: "test")
+    expect(user1.valid?).must_equal true
+
+    user2 = users(:ron)
+    expect(user2.valid?).must_equal true
+  end
+
+  it "will have the required fields" do
+    # Arrange
+
+    user = User.first
+    [:name].each do |field|
+
+      # Assert
+      expect(user).must_respond_to field
+    end
+  end
+
+  describe "validations" do
+    it "must have a name" do
+      # Arrange
+      user = User.create(name: "to_delete")
+      user.name = nil
+
+      # Assert
+      expect(user.valid?).must_equal false
+      expect(user.errors.messages).must_include :name
+      expect(user.errors.messages[:name]).must_equal ["can't be blank"]
+    end
+  end
+
 end

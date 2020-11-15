7e7ac2a540e2d59d18b0a6c416e947e91c881ce0
Madeline <madelign@gmail.com>
2020-11-14 22:04:09 -0500

    added header and nav links in layout template

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'homepages/index'
+  # get 'homepages/index'
   # get 'works/index'
   # get 'works/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'homepages#index'
 
   resources :works
 

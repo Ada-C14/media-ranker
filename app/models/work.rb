608508eddeb2e20db013a82ffdd4f1b38620e089
Kareha Agesa <kmaagesa@gmail.com>
2020-11-14 16:54:57 -0800

    fix homepage spotlight to not break when no works

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,13 +4,16 @@ class Work < ApplicationRecord
   has_many :users, through: :votes
 
   def self.select_spotlight
-    works = self.all
-    spotlight_votes = works.max_by{|work| work.votes.length}.votes.length
-    if spotlight_votes == 0
-      return nil
-    else
-      top_works = works.select{|work| work.votes.length == spotlight_votes}
-      top_work = top_works.sort_by{|work| work.title}[0]
+
+    if self.all.length > 0
+      works = self.all
+      spotlight_votes = works.max_by{|work| work.votes.length}.votes.length
+      if spotlight_votes == 0
+        return nil
+      else
+        top_works = works.select{|work| work.votes.length == spotlight_votes}
+        top_work = top_works.sort_by{|work| work.title}[0]
+      end
     end
     return top_work
   end
--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -10,5 +10,12 @@ describe HomepagesController do
 
   describe "index" do
 
+    it "doesnt break when there are no works" do
+      works(:test_work).destroy
+      expect(Work.count).must_equal 0
+      get root_url
+      must_respond_with :success
+    end
+
   end
 end

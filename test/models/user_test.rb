ddb5acfdca9ea4fd870406e51154af0ac3265e0a
Emily Nagai <emnagai89@gmail.com>
2020-11-15 15:36:16 -0800

    user logging in validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "logging in" do
+    it "validates that username is not blank" do
+      user = User.new(username: "")
+      user.username = nil
+
+      expect(user.valid?).must_equal false
+      expect(user.errors.messages).must_include :username
+      expect(user.errors.messages[:username]).must_equal ["can't be blank"]
+    end
+
+    it "validates that username is unique" do
+      user = users(:nagai)
+      user2 = User.create(username: 'Emily Nagai')
+
+      expect(user2.valid?).must_equal false
+      expect(user2.errors.messages[:username]).must_equal ["has already been taken"]
+    end
+  end
 end

1882e0ba06e4b82704145f657dea4faf37c7b670
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-11-14 20:03:34 -0800

    updated user and work models tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,8 +2,7 @@ require "test_helper"
 
 describe Work do
   it "can be instantiated" do
-    # Assert
-    expect(works(:book).valid?).must_equal true
+    expect(works(:work1).valid?).must_equal true
   end
 
   it "has required fields" do
@@ -12,18 +11,18 @@ describe Work do
     expect(work).must_respond_to :title
   end
 
-  describe "validations" do
+  describe "can be validated" do
     it "must have a title" do
       works(:book).title = nil
 
       expect(works(:book).valid?).must_equal false
       expect(works(:book).errors.messages).must_include :title
-      expect(works(:book).errors.messages[:title]).must_equal ["can't be blank"]
+
     end
 
   end
 
-  describe "relationships" do
+  describe "has valid relationships" do
     it "can have many votes" do
       expect(works(:work2).votes.first).must_be_instance_of Vote
       expect(works(:work2).votes.last).must_be_instance_of Vote
@@ -40,5 +39,5 @@ describe Work do
       expect(Work.spotlight.votes.count).must_equal 2
     end
   end
-  
+
 end

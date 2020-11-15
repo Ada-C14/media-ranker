2690cf3dfe086e28ae009a0436d29b400899dd90
Christina Minh <christina.minh@gmail.com>
2020-11-14 23:11:08 -0800

    Added VotesController#upvote tests

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -5,7 +5,6 @@ describe VotesController do
     it "creates a vote and redirects" do
       work = works(:dune)
 
-
       login()
 
       expect{
@@ -16,6 +15,24 @@ describe VotesController do
       must_respond_with :redirect
     end
 
+    it "does not create a vote for a work that doesn't exist and redirects" do
+      login()
 
+      expect{
+        post work_upvote_path(-1)
+      }.wont_change "Vote.count"
+
+      must_respond_with :redirect
+    end
+
+    it "does not create a vote when a user is not logged in" do
+      work = works(:dune)
+
+      expect{
+        post work_upvote_path(work)
+      }.wont_change "Vote.count"
+
+      must_respond_with :redirect
+    end
   end
 end

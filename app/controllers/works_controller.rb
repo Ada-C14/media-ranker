c9d37f1f756543267c6741e78f2b2df3a2e29590
Madeline <madelign@gmail.com>
2020-11-15 16:16:03 -0500

    added create work method and added flash display in layout template

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -5,4 +5,27 @@ class WorksController < ApplicationController
 
   def show
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      flash[:success] = "Work added successfully"
+      redirect_to root_path
+      return
+    else
+      flash.now[:error] = "Something happened. Work not added."
+      render :new, status: :bad_request
+      return
+    end
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end

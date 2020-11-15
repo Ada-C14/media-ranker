a17e2628a5de85233cd2e8e09fefa13f2161a20f
Jasmine <jasylop@gmail.com>
2020-11-14 16:15:02 -0800

    updated tests for top ten and spotlight for work

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,9 +2,7 @@ class Vote < ApplicationRecord
   validates :user_id, uniqueness: {scope: :work_id, message: "has already voted for this work"}
 
   belongs_to :work, counter_cache: true
-
   belongs_to :user
-
 
   def self.connect_work_user(work_id, user_id)
     return Vote.new(work_id: work_id, user_id: user_id)
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -4,7 +4,7 @@ describe Work do
 
   describe "validations" do
     it "is valid when all fields are present" do
-      work = Work.new(category: "book", title: "Harry Potter and the Prisoner of Azkaban", publication_year: "2000", description: "Harry return to hogwarts and is reunited with his Godfather")
+      work = Work.new(category: "book", title: "Big Friendship", publication_year: "2020", description: "A book about friendship")
       result = work.valid?
       expect(result).must_equal true
     end
@@ -30,19 +30,51 @@ describe Work do
 
   describe "custom methods" do
     describe "spotlight" do
-      it "returns a random work object" do
+      it "returns the most voted and the most recently recreated work" do
         expect(Work.spotlight).must_be_instance_of Work
+        expect(Work.spotlight).must_equal works(:hp1)
+
       end
     end
 
     describe "top ten" do
-      it "returns a list of 10 work objects for books" do
+      it "returns a list of 10 work objects for books sorted by number of votes then date created" do
         top_ten_books = Work.top_ten("book")
-        expect(top_ten_books.length).must_be :<=, 10
+        expect(top_ten_books.length).must_equal 10
         expect(top_ten_books.first).must_be_instance_of Work
         top_ten_books.each do |book|
           expect(book.category).must_equal "book"
         end
+        expect(top_ten_books.first).must_equal works(:hp1)
+        expect(top_ten_books.last).must_equal works(:DFZ1)
+      end
+
+      it "returns a list of work objects for movies sorted by number of votes then date created" do
+        top_ten_movies = Work.top_ten("movie")
+        expect(top_ten_movies.length).must_equal 4
+        expect(top_ten_movies.first).must_be_instance_of Work
+        top_ten_movies.each do |work|
+          expect(work.category).must_equal "movie"
+        end
+        expect(top_ten_movies.first).must_equal works(:mulan)
+        expect(top_ten_movies.last).must_equal works(:moana)
+      end
+
+      it "returns an empty list for albums" do
+        album = Work.top_ten("album")
+        expect(album.length).must_equal 0
+      end
+    end
+
+    def single_or_plural_votes
+      it "returns a singular 'vote' if there is 1 vote" do
+        work = works(:hp2)
+        expect(work.single_or_plural_votes).must_equal "1 Vote"
+      end
+
+      it "returns a plural 'votes' if there are 0 or 2+ votes" do
+        work = works(:hp1)
+        expect(work.single_or_plural_votes).must_equal "3 Votes"
       end
     end
   end

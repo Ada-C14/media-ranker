f116221b7f5b2ca098807278aba08ab15809ec9e
Marj E <marj@marjorieetienne.com>
2020-11-15 15:38:05 -0800

    added user controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    return :not_found if @user.nil?
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(User_params)
+    if @user.save
+      flash[:success] = "#{@user.title} was successfully added!"
+      redirect_to User_path(@user.id)
+      return
+    else
+      flash.now[:error] = "#{@user.title} was not successfully added!"
+      render :new, status: :bad_request
+      return
+    end
+
+  end
+
+  private
+
+  def user_params
+    return params.require(:user).permit(:name)
+  end
 end

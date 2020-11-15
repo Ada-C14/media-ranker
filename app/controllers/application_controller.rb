5923bbf1a09ac5b06f61da749828d4015548de98
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-14 23:07:39 -0800

    remove controller filter for user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :require_login
-
-  def require_login
-    # if session[:user_id] is nil, set error flash variable, redirect to login path
-    if session[:user_id].nil?
-      flash[:error] = "A problem occurred: Could not log in"
-      redirect_to login_path
-    end
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: [:login_form, :login]
 
   def index
     @users = User.all

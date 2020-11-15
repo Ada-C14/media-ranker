4acaef8f0e3436fc6593b3d184daa3d1a2b2bced
dnsrocha <drocha@gmail.com>
2020-11-14 18:15:33 -0800

    updated work and user models, implemented sort_by_category method to work model, updated class Work controller

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,15 @@
 class Work < ApplicationRecord
-  has_many :users
+  #has_many :users (that would be votes, I guess)
+
+  validates :category, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :creator, presence: true
+  validates :publication_year, presence: true
+  validates :description, presence: true
+
+
+  def self.sort_by_category(category)
+    self.where(category: category)
+  end
+
 end

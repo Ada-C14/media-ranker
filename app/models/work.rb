4463e45910949a636b27fe5fdc92d2223f46871c
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-14 18:22:32 -0800

    update top10 & spotlight on homepages and list order on works index

--- app/models/work.rb
+++ app/models/work.rb
@@ -8,7 +8,19 @@ class Work < ApplicationRecord
     return self.votes.count
   end
 
-  def spotlight
-    # work gets the most votes today
+  def self.index_order(category)
+    return self.where(category: category).joins(:votes).group("works.id").order("count_all desc, works.updated_at desc").count
+  end
+
+  def self.spotlight
+    return self.joins(:votes).group("works.id").order("count_all desc, works.updated_at desc").count.first
+  end
+  
+  def self.top_ten(category)
+    return self.where(category: category).joins(:votes).group("works.id").order("count_all desc, works.created_at desc").count.take(10)
+  end
+
+  def self.top_ten_helper(work_id)
+    return self.find_by(id: work_id)
   end
 end

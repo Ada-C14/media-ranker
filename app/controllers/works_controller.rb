78bcd840a4984db077a28a8bcd566f15e97eb392
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-14 21:56:04 -0800

    views

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,20 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all
+  end
 
+  def new
+    @work = Work.new
+  end
+
+  def show
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
+    if @work.nil?
+      # Change this to a 404 page
+      redirect_to works_path
+    end
+  end
 
   private
 

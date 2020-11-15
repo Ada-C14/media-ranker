6028baccc8f44098f03a0b944d850768a7a84b9c
Lina Do <l.do5147@gmail.com>
2020-11-15 08:25:01 -0800

    added index and show routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :votes, only: [:create]
 
 
+  resources :users, only: [:index, :show]
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"

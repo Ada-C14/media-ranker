c136a62060086b3892ba918ca6c8ec2b89f80b9f
Stacy <lundquist.stacy@gmail.com>
2020-11-14 23:32:11 -0800

    works_controller, work.rb update

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,6 @@
 class WorksController < ApplicationController
-  skip_before_action :find_work, only: [:new]
+  skip_before_action :find_work, except: [:show, :edit, :update, :destroy]
+
   def home
     @works = Work.all
     @all_works = Work.list_all_works
@@ -22,6 +23,47 @@ class WorksController < ApplicationController
     end
   end
 
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      flash[:success] = "Successfully created new #{@work.category} \"#{@work.title}!\""
+      redirect_to works_path
+    else
+      flash[:error] = "Work not created!"
+      @work.errors.messages.each do |field, messages|
+        flash[field] = messages
+      end
+      redirect_to new_work_path
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @work && @work.update(work_params)
+      flash[:success] = "Succesfully edited #{@work.category} \"#{@work.title}!\""
+      redirect_to work_path(@work.id)
+    elsif @work
+      flash.now[:error] = "Work edits not saved!"
+      @work.errors.messages.each do |field, messages|
+        flash[field] = messages
+      end
+      render :edit
+    end
+  end
+
+  def destroy
+    if @work.nil?
+      @deleted_work = @work.destroy
+      flash[:success] = "#{@work.title} deleted"
+      redirect_to root_path
+    else
+      flash[:failure] = "Work #{@work.title} not destroyed."
+      redirect_back fallback_location: root_path
+    end
+    end
+  end
 
   private
 

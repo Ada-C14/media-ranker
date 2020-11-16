66ff1dd450a6ca3d3d34ce981f9d03c360bea02e
Jessica Chan <jwinchan@gmail.com>
2020-11-16 06:02:16 -0800

    created tests for login method

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   post '/login', to: 'users#login'
   post '/logout', to: 'users#logout', as: 'logout'
 
+  get '/users/current', to: 'users#current', as 'current_user'
+
 end

893d18a45313b2970a569c60e1e6168fcef497e6
Sophia Donan <sophiadonan@gmail.com>
2020-11-16 00:02:04 -0800

    Wave1: Completed WORKS Controller, WORKS index; now has working (mostly complete) view all media page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,25 +1,72 @@
 class WorksController < ApplicationController
-  #Controller Actions are always methods
 
-  WORKS = [
-      { title: "Breakfast At Tiffanys", author: "someone", votes: 1, description: "a very average title"},
-      { title: "A very good title", author: "2someone", votes: 22, description: "a very average title"},
-      { title: "What is sleep?", author: "3someone", votes: 7, description: "a very average title"}
-  ]
+  before_action :find_work, except: [:index, :new, :create]
+
   def index
-    @works = WORKS
+    @works = Work.all.order(title: :asc)
+    @albums = Work.where(category: "album")
+    @books = Work.where(category: "book")
+    @movies = Work.where(category: "movie")
   end
 
   def show
-    id = params[:id].to_i
-    @work = WORKS[id]
-
     if @work.nil?
       head :not_found
       return
     end
-
-
   end
 
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      flash[:success] = "Created: #{@work.category}, ID: #{@work.id}"
+      redirect_to work_path(@work.id)
+      return
+    else
+      flash.now[:error] = "Uh Oh! Could not create #{@work.category}"
+      render new_work_path
+      return
+    end
+  end
+
+  def edit
+    if @work.nil?
+      flash.now[:error] = "Uh Oh! We couldn't find #{@work.category}"
+      redirect_to root_path
+      return
+    end
+  end
+
+  def update
+    if @work.nil?
+      flash.now[:error] = "Uh Oh! We couldn't find #{@work.category}"
+      redirect_to work_path
+      return
+    elsif @work.update(work_params)
+      flash[:success] = "Created: #{@work.category}: #{@work.title}"
+      redirect_to work_path(@work.id)
+      return
+    else # save failed
+    flash.now[:error] = "Uh Oh! We couldn't update #{@work.category}"
+    render :edit
+    return
+    end
+  end
+
+  def destroy
+    if @work.nil?
+      flash.now[:error] = "Uh Oh! We couldn't find #{@work.category}"
+      redirect_to works_path
+    else
+      @work.destroy
+      flash[:success] = "POOF! You have deleted: #{@work.category}: #{@work.title}"
+      redirect_to root_path
+      return
+    end
+  end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,4 @@
+
 class Work < ApplicationRecord
+
 end

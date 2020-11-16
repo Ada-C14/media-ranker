4230649b385bd4125d9b881967055e925d51ee65
dnsrocha <drocha@gmail.com>
2020-11-15 17:37:56 -0800

    fixed some configuration errors on RM / webpacker files

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ CSV.foreach(WORK_FILE, :headers => true) do |row|
   work.category = row['category']
   work.title = row['title']
   work.creator = row['creator']
-  work.publication = row['publication_year']
+  work.publication_year = row['publication_year']
   work.description = row['description']
   successful = work.save
   if !successful

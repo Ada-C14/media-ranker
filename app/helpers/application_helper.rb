87a973de4e8ca9b837528d31ea7caaffdef35408
Ren Carothers <rencarothers@gmail.com>
2020-11-16 12:29:45 -0800

    just getting more practice: adds testing for view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ module ApplicationHelper
   def readable_date(date)
     return "[unknown]" unless date
     # return content_tag(:span, "#{time_ago_in_words(date)} ago", class: 'date', title: date.to_s)
-    return date.strftime("%b %d, %Y")
+    # https://apidock.com/ruby/DateTime/strftime
+    return content_tag(:span, date.strftime("%b %d, %Y"), class: 'date', title: date.to_s)
   end
 end

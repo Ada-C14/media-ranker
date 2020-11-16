40c0d774b8f3f9ff5e7d51e0bb47ab864f8ed375
KayKay-git <kayalew@yahoo.com>
2020-11-15 21:40:02 -0500

    VOTE-TEST: Implemented validations testing, pass

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -23,7 +23,7 @@ class VoteTest < ActiveSupport::TestCase
       expect(valid_vote).must_respond_to :user_id
       expect(valid_vote).must_respond_to :work_id
     end
-    
+
     describe "relationships" do
       it "should have a user and work" do
         # Arrange
@@ -34,5 +34,38 @@ class VoteTest < ActiveSupport::TestCase
       end
     end
 
+    describe "validations" do
+      it  "must have a user" do
+        # Arrange
+        valid_work = works(:movie)
+        vote_with_nil_user = Vote.create(user: nil, work: valid_work)
+        # Assert
+        expect(vote_with_nil_user.valid?).must_equal false
+        expect(vote_with_nil_user.errors.messages).must_include :user
+        expect(vote_with_nil_user.errors.messages[:user]).must_equal ["must exist"]
+        assert_operator vote_with_nil_user.errors.count, :>, 0
+      end
+
+      it  "must have a work" do
+        # Arrange
+        valid_user = users(:user1)
+        vote_on_nil_work = Vote.create(user: valid_user, work: nil)
+        # Assert
+        expect(vote_on_nil_work.valid?).must_equal false
+        expect(vote_on_nil_work.errors.messages).must_include :work
+        expect(vote_on_nil_work.errors.messages[:work]).must_equal ["must exist"]
+        assert_operator vote_on_nil_work.errors.count, :>, 0
+
+      end
+
+      it "should not be saved if user already voted on work" do
+        # Arrange
+        already_voted = votes(:vote1)
+        invalid_new_vote = Vote.new(user_id: already_voted.user_id, work_id: already_voted.work_id)
+        # Assert
+        expect(invalid_new_vote.valid?).must_equal false
+        expect(already_voted).must_be_instance_of Vote
+      end
+    end
   end
 end

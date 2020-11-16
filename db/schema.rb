e02eb02bee7c5870f1f6252cc4556c942eefe094
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-16 07:35:17 -0800

    rollback successful after migration disaster, saving schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_15_020147) do
+ActiveRecord::Schema.define(version: 2020_11_15_015717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,11 +18,6 @@ ActiveRecord::Schema.define(version: 2020_11_15_020147) do
   create_table "users", force: :cascade do |t|
     t.string "user_name"
     t.date "join_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "votes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -4,70 +4,67 @@ require "test_helper"
 # Add Fixtures Later
 # New? Create?
 
-describe Work do
-  # before do
-  #
-  # end
-
-
-
-  it "can be instantiated and is valid when all required fields are present" do
-    # Arrange
-    @work = Work.new(title: "Long", category: "Movie")
-
-    # Act
-    created_work = @work.valid?
-
-    # Assert
-    expect(created_work).must_equal true
-  end
-
-  it "is invalid without title" do
-    # Arrange
-    @work = Work.new(category: "Movie")
-
-    #Act
-    created_work = @work.valid?
-
-    # Assert
-    expect(created_work).must_equal false
-    expect(@work.errors.messages).must_include :title
-  end
-
-  it "is invalid without category" do
-    # Arrange
-    @work = Work.new(title: "Long")
-
-    #Act
-    created_work = @work.valid?
-
-    # Assert
-    expect(created_work).must_equal false
-    expect(@work.errors.messages).must_include :category
-  end
-
-  it "returns a spotlight work" do
-    # spotlit = Work.spotlight
-    # @works = Work.all
-
-    expect(Work.spotlight).must_be_instance_of Work
-  end
-
-  it "returns nil for spotlight when database is empty" do
-    Work.delete_all
-    expect(Work.count).must_equal 0
-    expect(Work.spotlight).must_be_nil
-  end
-
-  it "returns an array for top 10" do
-    top_books = Work.top_ten("book")
-
-    expect(top_books).must_be_instance_of Array
-  end
-
-  it "returns an array of 10 books for top 10 method" do
-    top_books = Work.top_ten("book")
-
-    expect(top_books.length).must_equal 10
-  end
-end
+# describe Work do
+#   before do
+#     @work = works(:book1)
+#   end
+#
+#
+#
+#   it "can be instantiated and is valid when all required fields are present" do
+#     # Act
+#     created_work = @work.valid?
+#
+#     # Assert
+#     expect(created_work).must_equal true
+#   end
+#
+#   it "is invalid without title" do
+#     # Arrange
+#     @work.title = nil
+#
+#     #Act
+#     titleless_work = @work.valid?
+#
+#     # Assert
+#     expect(titleless_work).must_equal false
+#     expect(@work.errors.messages).must_include :title
+#   end
+#
+#   it "is invalid without category" do
+#     # Arrange
+#     @work.category = nil
+#
+#     #Act
+#     categoryless_work = @work.valid?
+#
+#     # Assert
+#     expect(categoryless_work).must_equal false
+#     expect(@work.errors.messages).must_include :category
+#   end
+#
+#   it "returns a spotlight work" do
+#     # spotlit = Work.spotlight
+#     # @works = Work.all
+#
+#     expect(Work.spotlight).must_be_instance_of Work
+#   end
+#
+#   it "returns nil for spotlight when database is empty" do
+#     Work.delete_all
+#     expect(Work.count).must_equal 0
+#     expect(Work.spotlight).must_be_nil
+#   end
+#
+#   it "returns an array for top 10" do
+#     top_books = Work.top_ten("book")
+#
+#     expect(top_books).must_be_instance_of Array
+#   end
+#
+#   it "returns an array of 10 books for top 10 method" do
+#     top_books = Work.top_ten("book")
+#
+#     expect(top_books.length).must_equal 10
+#   end
+# end

5135d860e37eaaa12b7aef7ceaf26ed8494d9479
Rachael Gomez <gomezrc1220@gmail.com>
2020-11-15 17:07:43 -0800

    media is sorted by number of votes

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
 
   get "/login", to: "users#login_form", as: "login"
+  get '/users', to: 'users#index', as: 'users'
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
   get "/users/current", to: "users#current", as: "current_user"

d6c9af591492b23cb91e0e008c3f578c50b895c2
Ida <ida.goitom@yahoo.com>
2020-11-15 20:22:09 -0800

    updated more indentations

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,31 +5,4 @@ class VotesController < ApplicationController
     @work.votes.create
     redirect_to(works_path)
   end
-  def index
-
-  end
-
-  def show
-
-  end
-
-  def new
-
-  end
-
-  def create
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  def destroy
-
-  end
 end

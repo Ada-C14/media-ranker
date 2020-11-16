5661c63ff1d77f67f04cfd43689ab6f5a63de98f
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-11-15 23:59:52 -0800

    created User Model tests, login passing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validation" do
+
+    before do
+      @user = User.create(username: 'Username')
+      @bad_user = User.create(username: "")
+    end
+
+    it "must have username to be valid " do
+      result = @user.valid?
+
+      expect(result).must_equal true
+    end
+  end
+
+  it "is invalid without a username" do
+    #Act
+    bad_result = @bad_user.valid?
+
+    expect(bad_result).must_equal false
+
+  end
+
+  it "is invalid if username already exists" do
+    new_user = User.new(username:'Username')
+
+    expect(new_user.valid?).must_equal false
+  end
+
+  describe "relationships" do
+
+  end
+
+  it "can have many votes" do
+
+  end
 end

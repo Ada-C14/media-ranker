48453dd87084c8eb91e64e52c04131d1a230646b
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-15 16:51:26 -0800

    works listed by cat

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,4 +3,14 @@ class Work < ApplicationRecord
 
   validates :title, presence: true
   validates :category, presence: true
+
+
+  def self.spotlight
+    return Work.all.sample(1)
+  end
+
+  def self.top_ten
+    return Work.all.sample(10)
+  end
+
 end

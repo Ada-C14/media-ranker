056184155d4f3b6a71bcd1e4a7c378bb55b8f229
marks214 <aimee.oz.x@gmail.com>
2020-11-15 21:41:20 -0800

    need to cascade deletes - if a user deletes a work, the vote needs to delete as well

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :destroy
   validates :category, presence: true, inclusion: { within: %w[movie book album] }
   validates :title, presence: true
   validates :creator, presence: true

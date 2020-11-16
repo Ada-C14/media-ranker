4ac35cabd5a101d903de3ee1b848638f5474df18
KayKay-git <kayalew@yahoo.com>
2020-11-15 19:18:11 -0500

    WORK-TEST: Implemented top ten custom method, pass

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -26,6 +26,56 @@ class WorkTest < ActiveSupport::TestCase
           expect(Work.spotlight).wont_be_nil
         end
       end
+
+      describe "top_ten" do
+        describe "top_ten_movies" do
+          it "should only have movie as its category" do
+            Work.top_ten("movie").each do |work|
+              expect(work.category).must_equal "movie"
+            end
+          end
+
+          it "should return a top ten list of movies sorted by ascending vote count" do
+            # Destroy all existing votes
+            Vote.destroy_all
+            # Create 3 unique votes for movie one (3 separate users)
+            Vote.create(work: works(:movie), user: users(:user1))
+            Vote.create(work: works(:movie), user: users(:user2))
+            Vote.create(work: works(:movie), user: users(:user3))
+
+            # Create 2 unique votes for movie two
+            Vote.create(work: works(:movie2), user: users(:user1))
+            Vote.create(work: works(:movie2), user: users(:user2))
+
+            # Create 1 vote for movie three
+            Vote.create(work: works(:movie3), user: users(:user1))
+
+
+            # Check to make sure votes are accurate
+            expect(works(:movie).votes.count).must_equal 3
+            expect(works(:movie2).votes.count).must_equal 2
+            expect(works(:movie3).votes.count).must_equal 1
+
+            # Check top ten list order
+            expect(Work.top_ten("movie").first).must_equal works(:movie)
+            expect(Work.top_ten("movie").last).must_equal works(:movie3)
+            expect(Work.top_ten("movie")).wont_be_nil
+
+          end
+
+          it "should return an empty list if the category has no works" do
+            # Destroy all existing works
+            Work.destroy_all
+            expect(Work.count).must_equal 0
+            # Since Work and Votes are associated, when work is destroyed, the the associated votes should also be destroyed.
+            expect(Vote.count).must_equal 0
+
+            expect(Work.top_ten("movie")).must_be_empty
+            expect(Work.top_ten("album")).must_be_empty
+            expect(Work.top_ten("book")).must_be_empty
+          end
+        end
+      end
     end
   end
 end

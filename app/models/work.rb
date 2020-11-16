2e85acdbfd5a3edd2fbf41bc36448f08e3977103
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-15 22:03:16 -0800

    spotlight finally working

--- app/models/work.rb
+++ app/models/work.rb
@@ -6,8 +6,12 @@ class Work < ApplicationRecord
 
 
   def self.spotlight
-    return Work.all.sample(1)
+    return Work.all.sample
   end
+
+  # def self.spotlight
+  #   return Work.all.max_by {|work| work.votes.count}
+  # end
 
   def self.top_ten
     return Work.all.sample(10)

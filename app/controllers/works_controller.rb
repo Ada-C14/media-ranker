ad254234af8654c5ff9decb6d53abea1e3502712
Sophia Donan <sophiadonan@gmail.com>
2020-11-15 19:39:23 -0800

    Created routes for show actions, made a works show page with model data(array)

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,13 +2,24 @@ class WorksController < ApplicationController
   #Controller Actions are always methods
 
   WORKS = [
-      { title: "Breakfast At Tiffanys", author: "someone"},
-      { title: "A very good title", author: "2someone"},
-      { title: "What is sleep?", author: "3someone"}
+      { title: "Breakfast At Tiffanys", author: "someone", votes: 1, description: "a very average title"},
+      { title: "A very good title", author: "2someone", votes: 22, description: "a very average title"},
+      { title: "What is sleep?", author: "3someone", votes: 7, description: "a very average title"}
   ]
   def index
     @works = WORKS
   end
 
+  def show
+    id = params[:id].to_i
+    @work = WORKS[id]
+
+    if @work.nil?
+      head :not_found
+      return
+    end
+
+
+  end
 
 end

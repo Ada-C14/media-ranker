5b31f1e7c5b9f7fe8a815362112e98ff82369302
Anna Kim <anna.kim93@gmail.com>
2020-11-15 20:41:38 -0500

    relationship tests for vote model

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,5 +1,22 @@
 require "test_helper"
 
 describe Vote do
+  let (:vote) { Vote.first }
 
+  it 'can be instantiated with the required fields' do
+    expect(vote.valid?).must_equal true
+    expect(vote).must_respond_to :user_id
+  end
+
+  describe 'relationships' do
+    it 'belongs to a user' do
+      vote
+      expect(Work.find_by(id: vote.work_id)).must_be_instance_of Work
+    end
+
+    it 'belongs to a work' do
+      vote
+      expect(User.find_by(id: vote.user_id)).must_be_instance_of User
+    end
+  end
 end

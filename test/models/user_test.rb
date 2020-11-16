e5825d3d6d543a6677d6917ed1c9f5581f210a63
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-11-16 00:50:01 -0800

    User validations/realtionships created and passing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,39 +1,42 @@
 require "test_helper"
 
 describe User do
-  describe "validation" do
+  let(:new_user){
+    User.new(username: "newuser")
+  }
+  
+  it "can be instantiated" do
+    expect(new_user.valid?).must_equal true
+  end 
 
+  describe "validation" do
     before do
-      @user = User.create(username: 'Username')
-      @bad_user = User.create(username: "")
+      @user = User.new(username: "test_user")
     end
 
     it "must have username to be valid " do  #Passing
       result = @user.valid?
-
       expect(result).must_equal true
+    end
+
+    it "is invalid without a username" do  #Passing
+      @user.username = nil
+      expect(@user.valid?).must_equal false
     end
   end
 
-  it "is invalid without a username" do
-    #Act
-    bad_result = @bad_user.valid?
+  # it "is invalid if username already exists" do
+  #   new_user = User.new(username:'Username')
+  #
+  #   expect(new_user.valid?).must_equal false
+  # end
 
-    expect(bad_result).must_equal false
+  describe "relationships" do
 
+    it "can have many votes" do
+      user1 = users(:user1)
+      expect(user1.votes.count).must_equal 2
   end
 
-  it "is invalid if username already exists" do
-    new_user = User.new(username:'Username')
-
-    expect(new_user.valid?).must_equal false
   end
-
-  # describe "relationships" do
-  #
-  # end
-  #
-  # it "can have many votes" do
-  #
-  # end
 end

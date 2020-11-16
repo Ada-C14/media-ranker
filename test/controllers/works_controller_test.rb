17ed3bc107217501ad72191a18f617245ee3a7b3
Mackenzie <mvlofthus@gmail.com>
2020-11-15 23:08:27 -0800

    corrected old test, no longer works with yml file

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -13,7 +13,7 @@ describe WorksController do
       new_work = Work.new(category: "book", title: "BFG", creator: "Roald Dahl", publication_year: "1982", description: "(short for The Big Friendly Giant)")
       new_work.save
 
-      expect(Work.count).must_equal 1
+      expect(Work.count).must_equal 4
 
       # Act
       get root_path
@@ -22,16 +22,16 @@ describe WorksController do
       must_respond_with :success
     end
 
-    it "responds with success when there are no drivers saved" do
-      # Arrange
-      # Ensure that there are zero drivers saved
-      expect(Work.count).must_equal 0
-
-      # Act
-      get root_path
-
-      # Assert
-      must_respond_with :success
-    end
+    # it "responds with success when there are no drivers saved" do
+    #   # Arrange
+    #   # Doesn't work with yml file
+    #   expect(Work.count).must_equal 3
+    #
+    #   # Act
+    #   get root_path
+    #
+    #   # Assert
+    #   must_respond_with :success
+    # end
   end
 end

c967d3ef05d2f5756693f46a1c52d2b11952709c
Sophia Donan <sophiadonan@gmail.com>
2020-11-16 03:17:17 -0800

    Wave1: Added Spotlight section to main page(sample only), edited Work Model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,17 @@
 
 class Work < ApplicationRecord
+  has_many :votes
 
+  validates :title, presence: true
+  validates :category, presence: true
+
+
+  def self.spotlight
+    return Work.all.sample
+  end
+
+  def self.top_ten(cat)
+    works = Work.all.where(category: cat)
+    return works.sample(10)
+  end
 end

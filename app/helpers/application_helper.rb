fbb8d8d781bf0b3eb63c48dd3af9c84b2199a1ae
Iris Lux <iris.lux0@gmail.com>
2020-11-16 01:50:04 -0800

    fixed error in display_details helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def display_detail(detail)
     if (detail.is_a? String)
-      return detail.empty? ? content_tag(:span, "Not Available", class: 'not_available')  : detail
+      return detail.empty? ? content_tag(:span, "N/A", class: 'not_available')  : detail
     elsif(detail.is_a? Integer)
       return detail
     elsif detail.nil?

b2714482e5170a2bc53f65c006967df31e2f823b
Sophia Donan <sophiadonan@gmail.com>
2020-11-15 22:12:55 -0800

    Deleted models, created new work model with terminal, made seeds.rb

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  has_many :votes
-  validates :username, presence: true
-end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,15 +1,2 @@
 class Work < ApplicationRecord
-  has_many :votes
-
-  validates :category, presence: true
-  validates :title, presence: true
-
-  def self.top_ten(category)
-    works = Work.all.where(category:category)
-    return works.max_by(10) { |work| work.votes.count }
-  end
-
-  def self.spotlight
-    return Work.all.max_by {|work| work.votes.count}
-  end
 end

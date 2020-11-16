6d1d71710b7b03c275c8c8cd18adfd946b47a817
Iris Lux <iris.lux0@gmail.com>
2020-11-16 01:34:52 -0800

    helper methods for single votes and not available info - implemented in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def display_detail(detail)
+    if (detail.is_a? String)
+      return detail.empty? ? content_tag(:span, "Not Available", class: 'not_available')  : detail
+    elsif(detail.is_a? Integer)
+      return detail
+    elsif detail.nil?
+      return content_tag(:span, "Not Available", class: 'not_available')
+    else
+      return detail
+    end
+  end
+
+  def display_votes(vote_count)
+    return vote_count == 1 ? "#{vote_count} Vote" : "#{vote_count} Votes"
+  end
 end

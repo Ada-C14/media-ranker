5495498546bd3c28016d8aa919c46ac27f3fbd61
Ida <ida.goitom@yahoo.com>
2020-11-15 17:54:47 -0800

    updated views for work to show the amt of votes of each media, updated form

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,7 +1,10 @@
 class WorksController < ApplicationController
+
+  before_action :find_work, only: [:edit, :update, :destroy, :upvote]
+
   def index
     @works = Work.all
-    #NEED TO DRY THIS ISH UP >>
+    #NEED TO DRY THIS UP >>
     @all_movies = Work.total_lists(category: "movie")
     @all_books = Work.total_lists(category: "book")
     @all_albums = Work.total_lists(category: "album")
@@ -36,7 +39,7 @@ class WorksController < ApplicationController
   end
 
   def edit
-    @work = Work.find_by(id: params[:id])
+    # @work = Work.find_by(id: params[:id])
     if @work.nil?
       redirect_to works_path
       return
@@ -44,13 +47,12 @@ class WorksController < ApplicationController
   end
 
   def update
-    @work = Work.find_by(id: params[:id])
+    # @work = Work.find_by(id: params[:id])
     if @work.nil?
       head :not_found
       return
     elsif  @work.update(work_params)
       flash[:success] = "This work was updated successfully"
-      #added this ^^
       redirect_to work_path
       return
     else
@@ -59,8 +61,8 @@ class WorksController < ApplicationController
   end
 
   def destroy
-    work_id = params[:id]
-    @work = Work.find_by(id: work_id)
+    # work_id = params[:id]
+    # @work = Work.find_by(id: params[:id])
     if @work.nil?
       head :not_found
       return
@@ -71,7 +73,7 @@ class WorksController < ApplicationController
   end
 
   def upvote
-    @work = Work.find_by(id: params[:id])
+    # @work = Work.find_by(id: params[:id])
     @user = User.find_by(id: session[:user_id])
 
     if @user.nil?
@@ -97,5 +99,10 @@ class WorksController < ApplicationController
 
   def work_params
     return params.require(:work).permit(:title, :description, :publication_date, :creator, :category)
+  end
+
+  def find_work
+    @work = Work.find_by(id: params[:id])
+
   end
 end

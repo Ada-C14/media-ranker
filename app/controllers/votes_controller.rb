16428bf7c6de34b55619e9de161167f4833ed4c9
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-17 05:47:03 -0800

    vote create action

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,27 @@
 class VotesController < ApplicationController
 
-  # def create
-  #   work_id = params[:work_id]
-  #   work = Work.find_by(id: work_id)
-  #
-  #   user_id = session[:user_id]
-  #
-  # end
+  def new
+    @vote = Vote.new
+  end
+
+  def create
+    work_id = params[:work_id]
+    work = Work.find_by(id: work_id)
+
+    user_id = session[:user_id]
+    user = User.find_by(id: session[:user_id])
+
+    @vote = Vote.new(work_id: work.id, user_id: user.id)
+
+    if @vote.save
+      flash[:success] = "Successfully upvoted"
+      redirect_to work_path(work.id)
+    else
+      flash[:error] = "There was a problem with counting your vote"
+      redirect_to work_path(work.id)
+    end
+
+  end
 
 
   private

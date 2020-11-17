7109e8a54879deb040c7bbb53523c17ccb9d698f
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-16 21:59:14 -0800

    added session routes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,12 @@
 class VotesController < ApplicationController
 
+  # def create
+  #   work_id = params[:work_id]
+  #   work = Work.find_by(id: work_id)
+  #
+  #   user_id = session[:user_id]
+  #
+  # end
 
 
   private

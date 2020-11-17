37426d738b6760a93362cb3801921739adba6712
Jessica Chan <jwinchan@gmail.com>
2020-11-17 00:37:54 -0800

    created fixtures for testing

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,8 @@
 class Work < ApplicationRecord
+  has_many :votes
   has_many :users, through: :votes
+
+  validates :category, presence:true
+  validates :title, presence: true, uniqueness: true
 
 end

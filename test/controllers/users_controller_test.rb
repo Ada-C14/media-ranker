7680e3e0d309d70d0e90eb4ebf73de34cef77074
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-17 09:01:39 -0800

    current user tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
+  # it "current user can return a page if the user is logged in" do
+  #   # Arrange
+  #   login_path
+  #
+  #   # Act
+  #   get current_user_path
+  #
+  #   # Assert
+  #   must_respond_with :success
+  #
   # end
+  # How to adapt what Chris did?
+
+  it "current user redirects us to login page if user isn't logged in" do
+    # Arrange
+    # No current user
+
+    # Act
+    get current_user_path
+
+    # Assert
+    must_respond_with :redirect
+    expect(flash[:error]).must_equal "You must log in"
+  end
 end

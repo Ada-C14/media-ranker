c5b093588bd3cbfc6a7e8a8d1d491f16cc2f3280
Jessica Chan <jwinchan@gmail.com>
2020-11-17 04:12:01 -0800

    added columns to vote table

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,24 +4,20 @@ class VotesController < ApplicationController
     @current_user = User.find_by(id: session[:user_id])
     if @current_user.nil?
       flash[:error] = "A problem occured: You must log in to do that"
-      redirect_back fallback_location: works_path
-      return
     else
       @work = Work.find_by(id: params[:work_id])
       @vote = Vote.new(
-          user_id: @current_user.id,
+          user_id: session[:user_id],
           work_id: @work.id
       )
       if @vote.save
         flash[:success] = "Successfully upvoted!"
-        redirect_back fallback_location: works_path
-        return
       else
         flash[:error] = "Could not upvote"
-        redirect_back fallback_location: works_path
-        return
       end
     end
+    redirect_to works_path
+    return
   end
 
 end

3f04e2e57153dc7ad89791d278a908e07e8f55f3
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-16 21:51:35 -0800

    votes nested route

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   root to: "homepages#index"
 
   resources :homepages, only: [:index]
-  resources :works
-
-  # resources :votes  << nested in works?
+  resources :works do
+    resources :votes, only: [:create] # destroy?
+  end
 
   resources :users, except: [:edit, :update, :destroy]
 end

75a7e20507386231ee4e7bf442ff8f850dafbb72
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-17 06:19:46 -0800

    migrations relating votes to user and work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  # returns the current user, or the user matching the user_id of the current session
+  def current_user
+    if session[:user_id]
+      return User.find_by(id: session[:user_id])
+    end
+  end
+
+
+
 end

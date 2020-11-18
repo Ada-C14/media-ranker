9e474ae45cbbc6ad595a153eedb5e335d3f58933
Sandy <sandyvasquez9@gmail.com>
2020-11-17 21:50:54 -0800

    renamed method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    # @users = User.order(date_joined :asc)
+    @users = User.order(date_joined :asc)
     @user = User.all
   end
 
@@ -20,9 +20,9 @@ class UsersController < ApplicationController
     name = params[:user][:name] #user input
     user = User.find_by(name: name)
 
-    if user #if !user.nil?
+    if !user.nil?  #if user
       session[:user_id] = user.id #setting the session id to the user_id
-      flash[:success] = "Successfully logged in as returning user #{name}"
+      flash[:success] = "Successfully logged in as returning user #{user.name}"
     else
       user = User.create(name: name, date_joined: Date.today)
       session[:user_id] = user.id

63104fa1a08ebd60f30f3d78a0b1dacfc9d839b9
Alice Boone <alicegbonel@gmail.com>
2020-11-27 18:00:08 -0800

    update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::Base
 
+  before_action :current_user
+
   def current_user
     return @current_user = User.find_by(id: session[:user_id])
   end
 
   def require_login
     if current_user.nil?
-      flash[:error]  = "A problem occurred: You must log in to do that"
-      redirect_to login_path
+      flash[:error] = "A problem occurred: You must log in to do that"
+      redirect_to root_path
     end
   end
 end

134a60350ba8c077f78cd25998b8ec1c2eaf7381
Hanh Solo <minhhanhnyc@gmail.com>
2020-11-27 20:37:12 -0800

    User index and user controller logout method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  # before_action :set_user 
+
   def index
     @users = User.all # list all users in order
   end
@@ -40,26 +42,32 @@ class UsersController < ApplicationController
   
 
   def logout
-    session[:user_id] = nil
-    flash[:success] = "Successfully logged out"
-    redirect_to root_path
-    return
-  end
-
-  def current
-    @current_user = User.find_by(id: session[:user_id])
-    unless @current_user
-      flash[:error] = "You must be logged in to see this page"
-      redirect_to root_path
+    if session[:user_id]
+      user = current_user()
+      unless user.nil?
+        session[:user_id] = nil
+        flash[:success] = "Goodbye #{user.name}"   
+      else
+        session[:user_id] = nil
+        flash[:error] = "Unknow User"
+      end
+    else
+      flash[:error] = "You must be logged in to logout"
       return
     end
   end
-  
-  private
 
-  def user_params
-    return params.require(:user).permit(:name)
-  end
+  
+
+  # private
+
+  # def set_user
+  #   @current_user = User.find_by(id: session[:user_id])
+  # end
+
+  # def user_params
+  #   return params.require(:user).permit(:name)
+  # end
   
 end
 

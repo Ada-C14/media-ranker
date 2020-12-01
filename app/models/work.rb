af0e01e7ec5d16abfe91f99642968ae57b76e928
Schanen Ryan <schanen.r@gmail.com>
2020-11-30 21:50:41 -0800

    merge conflict

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,6 @@
 class Work < ApplicationRecord
 
-  has_many :votes
+  has_many :votes, dependent: :destroy
   has_many :users, through: :votes
 
   validates :title, presence: true, uniqueness: {scope: :category}
@@ -9,7 +9,9 @@ class Work < ApplicationRecord
   validates :creator, presence: true
   validates :category, presence: true, inclusion: { in: %w(album book movie), message: "category must be a movie, book or album" }
 
+
   def self.order_media(category)
     where(category: category).order(vote_count: :desc)
   end
+
 end

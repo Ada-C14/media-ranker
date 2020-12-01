8e97c490679c3cdabd3277b7153dce48f3e04d7e
Sophia Donan <sophiadonan@gmail.com>
2020-11-30 20:14:26 -0800

    Wave2: Finished controller tests for logging in

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,19 +9,25 @@ describe UsersController do
 
   describe "logging in" do
     it "can login a new user" do
-      user_hash = {
-          user: {
-              username: "grace hopper"
-          }
-      }
-      expect{
-      post login_path, params: user_hash
+      user = nil
+      expect {
+        user = login()
       }.must_differ "User.count", 1
 
       must_respond_with :redirect
-      user = User.find_by(username: user_hash[:user][:username])
 
       expect(user).wont_be_nil
+      expect(session[:user_id]).must_equal user.id
+      expect(user.username).must_equal user_hash[:user][:username]
+    end
+
+    it "can log in an existing user" do
+      user = User.create(username: "Adele")
+
+      expect {
+        login(user.username)
+      }.wont_change "Users.count"
+
       expect(session[:user_id]).must_equal user.id
       expect(user.username).must_equal user_hash[:user][:username]
     end

e0306a3c092b6341f9a71c790f84eca3e954f5e6
Madeline <madelign@gmail.com>
2020-12-01 11:00:30 -0500

    added current user test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -54,4 +54,31 @@ describe UsersController do
       expect(session[:user_id]).must_be_nil
     end
   end
+
+  describe "current user" do
+    it "can return the page if user is logged in" do
+      # arrange
+      login()
+
+      # act
+      get current_user_path
+
+      # assert
+      must_respond_with :success
+    end
+
+    it "redirects us back if the user is not logged in" do
+      # no arrange step
+      # not logged in
+
+      # act
+      get current_user_path
+
+      # assert
+      must_respond_with :redirect
+      expect(flash[:error]).must_equal "You must be logged in to view this page"
+
+    end
+  end
+
 end

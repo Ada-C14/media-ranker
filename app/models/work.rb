7dce16eb7195d0b443b5524361f1ff3247e471b8
Madeline <madelign@gmail.com>
2020-12-01 21:41:27 -0500

    made work model code DRY

--- app/models/work.rb
+++ app/models/work.rb
@@ -10,30 +10,16 @@ class Work < ApplicationRecord
         return sorted_work.last
     end
 
-    def self.sort_albums
-        sorted_albums = self.all.select {|work| work.category == "album" }
-        return sorted_albums
-    end
-
     def self.sort_books
-        sorted_books = self.all.select {|work| work.category == "book" }
-        return sorted_books
+        books = Work.where(category: "book")
+        sorted_books = books.sort_by {|book| book.votes.count }
+        return sorted_books.reverse
     end
 
-    def self.top_books
-        top_books = sort_books.sort_by {|work| work.votes.count }
-
-        top_ten_books = top_books.last(10).reverse
-
-        return top_ten_books
-    end
-
-    def self.top_albums
-        top_albums = sort_albums.sort_by {|work| work.votes.count }
-
-        top_ten_albums = top_albums.last(10).reverse
-
-        return top_ten_albums
+    def self.sort_albums
+        albums = Work.where(category: "album")
+        sorted_albums = albums.sort_by {|album| album.votes.count }
+        return sorted_albums.reverse
     end
 
 end
